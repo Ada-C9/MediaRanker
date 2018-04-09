f64917d9b9fd0497c13a026708cc4712b884d297
Wenjie <traxiaojie@gmail.com>
2018-04-09 16:46:18 -0700

    Created model vote Added foreign key work and user in model vote

--- app/models/category.rb
+++
@@ -1,4 +0,0 @@
-class Category < ApplicationRecord
-  validates :name, presence: true 
-  has_and_belongs_to_many :works
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409222227) do
+ActiveRecord::Schema.define(version: 20180409234327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "work_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+    t.index ["work_id"], name: "index_votes_on_work_id"
   end
 
   create_table "works", force: :cascade do |t|
@@ -34,13 +37,9 @@ ActiveRecord::Schema.define(version: 20180409222227) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
-  create_table "works_categories", force: :cascade do |t|
-    t.bigint "work_id"
-    t.bigint "category_id"
-    t.index ["category_id"], name: "index_works_categories_on_category_id"
-    t.index ["work_id"], name: "index_works_categories_on_work_id"
-  end
-
+  add_foreign_key "votes", "users"
+  add_foreign_key "votes", "works"
 end
