e73d08f8433cfa79ff80d29af182772be7982214
emilcecarlisa <emilcecarlisa@gmail.com>
2018-04-09 16:46:12 -0700

    Created the function in seed file to convert csv to seedable data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# input is markup, csv File
+# output is a hash in an array where the key in header or first row
+
+def load_media()
+  all_works = []
+
+  works = CSV.open('media_seeds.csv', headers: true)
+
+  works.each do |work|
+    work[:category] = work[0]
+    work[:title] = work[1]
+    work[:created_by] = work[2]
+    work[:publication_date] = work[3]
+    work[:description] = work[4]
+
+    all_works << Work.new(work)
+  end
+
+  return all_works
+end
