db67fd9da97b392c9448c891c627c427ed75e7bb
Ari <dominiquearia0@gmail.com>
2018-04-09 16:57:31 -0700

    Added column category to works

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,39 @@
 class WorksController < ApplicationController
+
+  def index
+    @works.Work.all
+  end
+
+  def new
+    @work = Work.new(work_params)
+  end
+
+  def create
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to works_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @works = Work.
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def work_params
+    return params.require(:work).permit(:title, :publication_year, :description, :creator, :category)
+  end
+  end
+
 end
