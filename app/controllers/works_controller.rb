a05f61352d70eecbd6e45ad950ee747cf7863eae
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-04-09 16:32:16 -0700

    Refactored index method and view

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,8 @@
 class WorksController < ApplicationController
   def index
-    @works = Work.all.order(id: :asc)
+    @albums = Work.where(category:"album")
+    @books = Work.where(category:"book")
+    @movies = Work.where(category:"movie")
   end
 
   def show
