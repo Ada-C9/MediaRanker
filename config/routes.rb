8da526788c2e51ac440a0c9dc1f7bc2de1eb699d
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-04-09 16:06:03 -0700

    Created all users,votes,works controllers and user,vote and work models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,45 @@
 Rails.application.routes.draw do
+  get 'votes/index'
+
+  get 'votes/show'
+
+  get 'votes/new'
+
+  get 'votes/create'
+
+  get 'votes/edit'
+
+  get 'votes/update'
+
+  get 'votes/destroy'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
+  get 'works/index'
+
+  get 'works/show'
+
+  get 'works/new'
+
+  get 'works/create'
+
+  get 'works/edit'
+
+  get 'works/update'
+
+  get 'works/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
