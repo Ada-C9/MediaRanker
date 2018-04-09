4f0c5e110fb450eca70525c26c97fed6b64d29c5
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-04-09 15:57:29 -0700

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+MEDIA_FILE = Rails.root.join('db', 'media_seeds.csv')
+puts "Loading raw work data from #{MEDIA_FILE}"
+
+work_failures = []
+CSV.foreach(MEDIA_FILE, :headers => true) do |row|
+  work = Work.new
+  work.category = row['category']
+  work.title = row['title']
+  work.creator = row['creator']
+  work.publication_year = row['publication_year']
+  work.description = row['description']
+  puts "Created work: #{work.inspect}"
+  successful = work.save
+  if !successful
+    work_failures << work
+  end
+end
+
+puts "Added #{Work.count} work records"
+puts "#{work_failures.length} works failed to save"
