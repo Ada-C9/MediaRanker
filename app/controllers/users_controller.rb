b242f0b04eab0bbd9ee62236cf09de4aba28df88
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-04-09 16:08:20 -0700

    Basic methods roughed out for Books, Users, and Votes controllers.  Very much subject to change.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,68 @@
+class UsersController < ApplicationController
+
+    def index
+      @users = User.all
+    end
+
+    def show
+      id = params[:id]
+      @user = User.find(id)
+    end
+
+    def new
+      @user = User.new
+    end
+
+    def create
+      user = User.new(user_params)
+
+      if user.save #it worked
+        redirect_to users_path
+      else
+        render :new
+      end
+    end
+
+    def edit
+      @user = User.find_by(id: params[:id])
+      if @user.nil?
+        redirect_to users_path
+      end
+    end
+
+    def update
+      @user = User.find_by(id: params[:id])
+      if !@user.nil?
+
+        # Materials commented out because we are now using strong params.
+        # @book.update(title: params[:book][:title], author: params[:book][:author], description: params[:book][:description])
+
+        if @user.update(user_params)
+          redirect_to user_path(@user.id)
+        else
+          render :edit
+        end
+      else
+        redirect_to users_path
+      end
+    end
+
+    def destroy
+      id = params[:id]
+      @user = User.find(id)
+
+      if @user.destroy #it worked
+        redirect_to users_path
+      else
+        render :destroy
+      end
+    end
+
+    private
+
+    def user_params
+      # You don't need an explicit return here, but if it helps keep things straight in your head, it's okay to do it.
+      params.require(:user, :name).permit(vote_ids: [])
+    end
+  end
+end
---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,67 @@
+class WorksController < ApplicationController
+
+  def index
+    @works = Work.all
+  end
+
+  def show
+    id = params[:id]
+    @work = Work.find(id)
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    work = Work.new(work_params)
+
+    if work.save #it worked
+      redirect_to works_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      redirect_to works_path
+    end
+  end
+
+  def update
+    @work = Work.find_by(id: params[:id])
+    if !@work.nil?
+
+      # Materials commented out because we are now using strong params.
+      # @book.update(title: params[:book][:title], author: params[:book][:author], description: params[:book][:description])
+
+      if @work.update(work_params)
+        redirect_to work_path(@work.id)
+      else
+        render :edit
+      end
+    else
+      redirect_to works_path
+    end
+  end
+
+  def destroy
+    id = params[:id]
+    @work = Work.find(id)
+
+    if @work.destroy #it worked
+      redirect_to index_path
+    else
+      render :destroy
+    end
+  end
+
+  private
+
+  def work_params
+    # You don't need an explicit return here, but if it helps keep things straight in your head, it's okay to do it.
+    params.require(:work, :category, :title).permit(:creator, :publication_year, :description, vote_ids: [])
+  end
+end
