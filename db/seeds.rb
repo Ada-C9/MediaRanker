7257f3b33948f48a2f5aaed9859188eb960a8297
Brenda Rios <riosbrenda@gmail.com>
2018-04-09 15:46:09 -0700

    Updated models and did migration.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+WORK_FILE = Rails.root.join('db', 'media_seeds.csv')
+puts "Loading raw work data from #{WORK_FILE}"
+
+work_failures = []
+
+CSV.foreach(WORK_FILE, :headers => true) do |row|
+  work = Work.new
+  work.category = row['category']
+  work.title = row['title']
+  work.creator = row['creator']
+  work.publication_year = row['publication_year']
+  work.description = row['description']
+
+  puts "Created work: #{work.inspect}"
+  successful = work.save
+
+  if !successful
+    work_failures << work
+  end
+end
+
+puts "Added #{Work.count} work records"
+puts "#{work_failures.length} works failed to save"
