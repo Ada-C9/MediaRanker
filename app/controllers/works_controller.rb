9971cec443191f92964b54af71f5cb088e48f1f3
Samantha Berk <sberk10@gmail.com>
2018-04-09 16:56:02 -0700

    Add #new and #create with strong params to works_controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -6,9 +6,16 @@ class WorksController < ApplicationController
   end
 
   def new
+    @work = Work.new
   end
 
   def create
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to work_path(work)
+    else
+      render :new
+    end
   end
 
   def show
@@ -21,6 +28,12 @@ class WorksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
   end
 
 end
