68c4f672eface6ffb2fa4928ecaaf0e1b3bc1b96
Ari <dominiquearia0@gmail.com>
2018-04-09 16:09:37 -0700

    new model for works

---
+++ db/migrate/20180409230359_create_works.rb
@@ -0,0 +1,12 @@
+class CreateWorks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :works do |t|
+      t.string :title
+      t.string :creator
+      t.date :publication_year
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
