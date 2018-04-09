e3274a4c3187f8307120651b2d42b9fd2193fbfc
Winifred Irarrazaval <winirarrazaval@gmail.com>
2018-04-09 14:58:02 -0700

    decided which routes they are going to have

--- app/controllers/work_controller.rb
+++ app/controllers/work_controller.rb
@@ -4,5 +4,20 @@ class WorkController < ApplicationController
     @works = Work.all
   end
 
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end 
+
 
 end
