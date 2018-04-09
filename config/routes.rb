b52eda2fec9f16b10a31beef8527b0e297340570
Nora Peters <norapeters5@gmail.com>
2018-04-09 16:30:07 -0700

    adds resources for users, works, and votes

--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,10 @@
 Rails.application.routes.draw do
-  get 'votes/index'
 
-  get 'votes/show'
+  # root 'welcome#index'
 
-  get 'votes/new'
+  resources :users
 
-  get 'votes/create'
+  resources :works
 
-  get 'votes/edit'
-
-  get 'votes/update'
-
-  get 'votes/destroy'
-
-  get 'works/index'
-
-  get 'works/show'
-
-  get 'works/new'
-
-  get 'works/create'
-
-  get 'works/edit'
-
-  get 'works/update'
-
-  get 'works/destroy'
-
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/edit'
-
-  get 'users/update'
-
-  get 'users/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :votes
 end
