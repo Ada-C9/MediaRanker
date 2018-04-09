b66ecbeb6b23fde52493ebbbe193dbe305e6bb67
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-04-09 16:49:18 -0700

    Created welcome controller and set-up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,10 @@
 Rails.application.routes.draw do
-  get 'votes/index'
 
-  get 'votes/show'
 
-  get 'votes/new'
+  root 'welcomes#index'
 
-  get 'votes/create'
+  resources :users
+  resources :works
+  resources :votes
 
-  get 'votes/edit'
-
-  get 'votes/update'
-
-  get 'votes/destroy'
-
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/edit'
-
-  get 'users/update'
-
-  get 'users/destroy'
-
-  get 'works/index'
-
-  get 'works/show'
-
-  get 'works/new'
-
-  get 'works/create'
-
-  get 'works/edit'
-
-  get 'works/update'
-
-  get 'works/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
