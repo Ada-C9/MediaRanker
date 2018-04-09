4ef3b7706140c74a7739b972a836eb266612646e
Samantha Berk <sberk10@gmail.com>
2018-04-09 16:04:19 -0700

    Fixed load error for seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'csv'
-WORKS_FILE = Rails.root.join('db', 'seed_data', 'media_seeds.csv')
+WORKS_FILE = Rails.root.join('db', 'media_seeds.csv')
 puts "Loading works data from #{WORKS_FILE}"
 
 work_failures = []
