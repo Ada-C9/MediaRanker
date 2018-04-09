60af80e4ab6e4256e490d6362e3ba6e867645e7c
Tor Shimizu <torshimizu@gmail.com>
2018-04-09 16:20:55 -0700

    seeded development database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'CSV'
+
+FILE = 'db/media_seeds.csv'
+
+unsaved = []
+CSV.read(FILE, headers: true).each do |row|
+  work = Work.new
+  work.category = row['category']
+  work.title = row['title']
+  work.creator = row['creator']
+  work.publication_year = row['publication_year'].to_i
+  work.description = row['description']
+
+  successful = work.save
+  if !successful
+    unsaved << work
+  end
+
+  puts "#{unsaved.length} works not saved"
+end
