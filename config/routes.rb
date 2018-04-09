9f90c161292d7ce35851d6481c43f8dc86e0aafe
Zheng Cao <chori84@gmail.com>
2018-04-09 15:00:58 -0700

    Generate controllers and model for Work, run migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'works/index'
+
+  get 'works/show'
+
+  get 'works/new'
+
+  get 'works/create'
+
+  get 'works/edit'
+
+  get 'works/update'
+
+  get 'works/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
