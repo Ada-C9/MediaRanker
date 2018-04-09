3df61d713afb1a9ae0535432b7047f39c69b5a77
Kaitlin Forsman <kcforsman@outlook.com>
2018-04-09 16:49:10 -0700

    generated controllers for homepage and publications

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  get 'publications/index'
+
+  get 'publications/show'
+
+  get 'publications/new'
+
+  get 'publications/create'
+
+  get 'publications/edit'
+
+  get 'publications/update'
+
+  get 'publications/destroy'
+
+  get 'homepage/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/publications_controller_test.rb
@@ -0,0 +1,39 @@
+require "test_helper"
+
+describe PublicationsController do
+  it "should get index" do
+    get publications_index_url
+    value(response).must_be :success?
+  end
+
+  it "should get show" do
+    get publications_show_url
+    value(response).must_be :success?
+  end
+
+  it "should get new" do
+    get publications_new_url
+    value(response).must_be :success?
+  end
+
+  it "should get create" do
+    get publications_create_url
+    value(response).must_be :success?
+  end
+
+  it "should get edit" do
+    get publications_edit_url
+    value(response).must_be :success?
+  end
+
+  it "should get update" do
+    get publications_update_url
+    value(response).must_be :success?
+  end
+
+  it "should get destroy" do
+    get publications_destroy_url
+    value(response).must_be :success?
+  end
+
+end
