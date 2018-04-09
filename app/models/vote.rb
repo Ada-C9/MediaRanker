9d9213818b58146e385548069cd49a378687e272
Hannah Cameron <hannahiscoding@gmail.com>
2018-04-09 15:05:31 -0700

    filled out user, vote and work models. created files for controllers

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,2 +1,4 @@
 class Vote < ApplicationRecord
+  belongs_to :user
+  belongs_to_and_has_many :works
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,7 @@
 class Work < ApplicationRecord
+  belongs_to_and_has_many :votes
+
+  validates :category, presence: true
+            :title, presence: true
+
 end
