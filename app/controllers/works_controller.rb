3a1097839d1317bea8e583a7742bc1bfdc10b919
Kiera <kiera.thomasson@gmail.com>
2018-04-09 14:57:55 -0700

    Built out CRUD method in controller, set up restful routes and created an index view file for work

---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,43 @@
+class WorksController < ApplicationController
+
+  def index
+    @works = Work.all
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    work = Work.new(work_params)
+  end
+
+  def show
+    work_id = params[:id]
+
+    @work = Work.find(work_id)
+  end
+
+  def edit
+    @work = Work.find(params[:id])
+  end
+
+  def update
+    work = Work.find(params[:id])
+
+    work.assign_attributes(work_params)
+
+    if work.save
+      redirect_to work_path(work)
+  end
+
+  def destroy
+    @work = Work.find(params[:id])
+
+    redirect_to works_path
+  end
+
+private
+def work_params
+  return params.require(:work).permit(:category, :title, :created_by, :votes, :published_year, :description)
+end
