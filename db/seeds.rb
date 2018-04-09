3cc1080cbe6038b4afd8b588d7474242ef60c8bd
Karinna Iniguez <karinnainiguez@gmail.com>
2018-04-09 15:28:29 -0700

    Add routes and seed data from seed file.  Test success of seed data using works#index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+categories = ["album", "book", "movie"]
+
+categories.each do |c|
+  Category.create!( name: c )
+end
+
+
+
+WORKS_FILE = Rails.root.join('db','seed_data', 'media_seeds.csv')
+
+work_failures = []
+
+CSV.foreach(WORKS_FILE, :headers => true) do |row|
+  work = Work.new
+  work.category = Category.find_by(name: row['category'])
+  work.title = row['title']
+  work.creator = row['creator']
+  work.publication_year = row['publication_year']
+  work.description = row['description']
+
+  successful = work.save
+  if !successful
+    work_failures << work
+    puts "Failed to save work: #{work.inspect}"
+  else
+    puts "Created work: #{work.inspect}"
+  end
+
+end
+
+puts "Added #{Work.count} work records"
+puts "#{work_failures.length} works failed to save"
