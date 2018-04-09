eca3bb2adf389d69beee04e6b2989dfb5a1fd917
LetiTran <leticiastran@gmail.com>
2018-04-09 15:34:05 -0700

    Gneretaed Works, Users and Votes controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,54 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  # Works
+  get 'works/', to: 'works#index', as: 'works'
+
+  get 'works/new', to: 'works#new', as: 'new_work'
+
+  post 'works', to: 'works#create'
+
+  get 'works/:id/edit', to: 'works#edit', as: 'edit_work'
+
+  get 'works/id', to: 'works#show', as: 'work'
+
+  patch 'works/:id', to: 'works#update'
+
+  delete 'works/:id', to: 'works#destroy'
+
+
+  # Users
+  get 'users/', to: 'users#index', as: 'users'
+
+  get 'users/new', to: 'users#new', as: 'new_user'
+
+  post 'users', to: 'users#create'
+
+  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
+
+  get 'users/id', to: 'users#show', as: 'user'
+
+  patch 'users/:id', to: 'users#update'
+
+  delete 'users/:id', to: 'users#destroy'
+
+
+  # Votes
+  get 'votes/', to: 'votes#index', as: 'votes'
+
+  get 'votes/new', to: 'votes#new', as: 'new_vote'
+
+  post 'votes', to: 'votes#create'
+
+  get 'votes/:id/edit', to: 'votes#edit', as: 'edit_vote'
+
+  get 'votes/id', to: 'votes#show', as: 'vote'
+
+  patch 'votes/:id', to: 'votes#update'
+
+  delete 'votes/:id', to: 'votes#destroy'
+
+
+
+
 end
