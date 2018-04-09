0bc855083b32a27417d6cdd12904f9d8028e4616
Brenda Rios <riosbrenda@gmail.com>
2018-04-09 16:28:15 -0700

    Added new and show methods in works controller.

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -3,4 +3,21 @@ class WorksController < ApplicationController
   def index
     @works = Work.all
   end
+
+  def new
+    @work = Work.new
+  end
+
+  def show
+    @work = Work.find(params[:id])
+  end
+
+
+
+  private
+  def driver_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
+
+
 end
