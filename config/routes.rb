7971a313e4fa95e9b6775e3eba8ac1dba1f5ef9f
Tor Shimizu <torshimizu@gmail.com>
2018-04-09 14:55:05 -0700

    added a MainpageController for the main index page of site

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'mainpage#index'
+  
   get '/books', to: 'works#index', as: 'books'
   resources :works do
     post '/upvote', to: 'works#upvote', as: 'upvote'
