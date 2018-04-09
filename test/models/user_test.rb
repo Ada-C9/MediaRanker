090554b686a9577162d2c74b4dc2dde50cd67e53
Abiaina <abinnet.a@gmail.com>
2018-04-09 16:35:19 -0700

    Initial Rails setup

---
+++ test/models/user_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+describe User do
+  describe 'validations' do
+    before do
+      # Arrange
+      user = User.new(username: 'test')
+
+  it "must have username" do
+  end
+end
