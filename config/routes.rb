ddfbd1759283f3b6a93f3c078f0bca4a99133aca
Dikla <dikla.aharoni@gmail.com>
2018-04-09 16:36:12 -0700

    Added works controller Added work model Added migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'works/index'
+
+  get 'works/new'
+
+  get 'works/create'
+
+  get 'works/show'
+
+  get 'works/update'
+
+  get 'works/edit'
+
+  get 'works/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
