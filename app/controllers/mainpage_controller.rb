4314deb97df880e1a771b38286d5973d9113da0d
Tor Shimizu <torshimizu@gmail.com>
2018-04-09 16:58:43 -0700

    added site icon and added some controller routes

--- app/controllers/mainpage_controller.rb
+++ app/controllers/mainpage_controller.rb
@@ -1,4 +1,7 @@
 class MainpageController < ApplicationController
   def index
+    @top_albums = Work.where(category: 'album')
+    @top_books = Work.where(category: 'book')
+    @top_movies = Work.where(category: 'movie')
   end
 end
