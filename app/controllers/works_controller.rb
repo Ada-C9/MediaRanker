a4ee202288b9335fda8b651b25965004a04e00c6
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-04-09 19:11:44 -0700

    Added work_params and show function

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,31 @@
 class WorksController < ApplicationController
 
   def index
+    @works = Work.all
   end
-  
+
+  def show
+    @work = Work.find(params[:id])
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work
+
+    if @work.save
+      redirect_to works_path
+    else
+      render :new
+    end
+
+  end
+
+private
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
+
 end
