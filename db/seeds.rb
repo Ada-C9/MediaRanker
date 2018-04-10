c7c84252107c3f74e9a24e7c5efb61acdc879d40
Kaitlin Forsman <kcforsman@outlook.com>
2018-04-10 12:08:54 -0700

    filled out seeds.rb and seeded the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+PUBLICATION_FILE = Rails.root.join('db', 'media_seeds.csv')
+puts "Loading raw publication data from #{PUBLICATION_FILE}"
+
+publication_failures = []
+CSV.foreach(PUBLICATION_FILE, :headers => true) do |row|
+  publication = Publication.new
+  publication.category = row['category']
+  publication.title = row['title']
+  publication.creator = row['creator']
+  publication.publication_year = row['publication_year']
+  publication.description = row['description']
+  successful = publication.save
+  if !successful
+    publication_failures << publication
+    puts "Failed to save publication: #{publication.inspect}"
+  else
+    puts "Created publication: #{publication.inspect}"
+  end
+end
+
+puts "Added #{Publication.count} publication records"
+puts "#{publication_failures.length} publication failed to save"
+
+puts "done"
