137e6931898a791dae4a4ccca45efc3d2c3bf64c
Anne <annehwatson@gmail.com>
2018-04-10 08:58:24 -0700

    work create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def new
+    @user = User.new
+  end
+
+  def vote
+  end
+
+  private
+
+  def user_params
+    return params.require(:user).permit(:name)
+  end
 end
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -11,9 +11,26 @@ class WorksController < ApplicationController
     @work = Work.new
   end
 
+  def create
+    @work = Work.new(work_params)
+
+    if @work.save
+      redirect_to "/works"
+    else
+      render :new
+    end
+  end
+
   def show
     work_id = params[:id]
 
     @work = Work.find_by(id: work_id)
   end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
+
 end
