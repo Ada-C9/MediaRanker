e20cbc6ff044f39019126235ba8a3bd81f1c861b
Kat <kat.eastman@gmail.com>
2018-04-10 16:25:56 -0700

    added vote_count to works model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180410223717) do
+ActiveRecord::Schema.define(version: 20180410232438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20180410223717) do
     t.string "creator"
     t.string "description"
     t.integer "publication_year"
+    t.integer "vote_count"
   end
 
   add_foreign_key "votes", "users"
