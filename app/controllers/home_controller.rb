5195e9b63034dd1c2c921c1e8756b6ce276b1e28
LetiTran <leticiastran@gmail.com>
2018-04-10 14:48:44 -0700

    Working on home-page layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
     @media = Work.all
+
+    @movies = Work.where(category: "movie")
+    @books = Work.where(category: "book")
+    @albums = Work.where(category: "album")
   end
 end
