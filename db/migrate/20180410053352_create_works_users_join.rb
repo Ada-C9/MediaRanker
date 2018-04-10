5d3fabdb0390802721510a50f1121a269a1521ad
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-04-09 22:34:51 -0700

    added join table

---
+++ db/migrate/20180410053352_create_works_users_join.rb
@@ -0,0 +1,9 @@
+class CreateWorksUsersJoin < ActiveRecord::Migration[5.1]
+  def change
+    create_table :works_users do |t|
+      t.belongs_to :work, index: true
+      t.belongs_to :user, index: true
+      t.timestamps
+    end
+  end
+end
