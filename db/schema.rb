c96204b413a5fc4b1fb7492aeb098679828e264b
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-04-09 17:26:15 -0700

    Created migration to change publication_year from date to integer type. Re-seeded database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409214423) do
+ActiveRecord::Schema.define(version: 20180410000409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,10 +19,10 @@ ActiveRecord::Schema.define(version: 20180409214423) do
     t.string "title"
     t.string "category"
     t.string "creator"
-    t.date "publication_year"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "publication_year"
   end
 
 end
