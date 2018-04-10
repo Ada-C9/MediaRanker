903f9dfac669842c5ac28ee67e9dda99dd46429d
Wenjie <traxiaojie@gmail.com>
2018-04-09 17:03:47 -0700

    Merge conflicts

--- db/migrate/20180409234327_add_column_to_vote.rb
+++ db/migrate/20180409234327_add_column_to_vote.rb
@@ -1,10 +1,6 @@
 class AddColumnToVote < ActiveRecord::Migration[5.1]
   def change
     add_reference :votes, :user, foreign_key: true
-<<<<<<< HEAD
-    add_reference :votes, :work, foreign_key: true
-=======
     add_reference :votes, :work, foreign_key: true  
->>>>>>> master
   end
 end
