64ede75bb693b1c444ae0bd8937379533f3a721e
Phoebe <phoebe.manning8@gmail.com>
2018-04-10 16:18:04 -0700

    refactors work model logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def index
+    @works = Work.all
     @hash = Work.make_category_hash
     # @runs = Work.num_runs
   end
--- app/models/work.rb
+++ app/models/work.rb
@@ -4,24 +4,19 @@ class Work < ApplicationRecord
   has_many :users, through: :votes
 
   def self.make_category_hash
-    category_hash = Hash.new
+    categories = []
     i = 0
     while i < Work.all.length
-      if category_hash.has_key?(Work.all[i].category) == false
-        category_hash[Work.all[i].category] = []
-        category_hash[Work.all[i].category] << Work.all[i]
-      # elsif category_hash.has_key?(Work.all[i].category) && category_hash[Work.all[i].category].length < 10
-      else
-        category_hash[Work.all[i].category] << Work.all[i]
+      if categories.include?(Work.all[i].category) == false
+        categories << Work.all[i].category
       end
       i += 1
     end
-    return category_hash
+    return categories
   end
 
   def self.num_runs(array)
     runs = 0
-    hash = Work.make_category_hash
     array.length > 10 ? runs = 10 : runs = array.length
     return runs
   end
