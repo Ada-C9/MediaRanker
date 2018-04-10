5b37ff1893eb3b65fd775b3ba587352683f35df4
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-04-09 18:38:34 -0700

    Added index view for all arts, divided it up by category

---
+++ app/controllers/arts_controller.rb
@@ -0,0 +1,11 @@
+class ArtsController < ApplicationController
+  def index
+    @arts = Art.all
+
+    @albums = Art.where(category: 'album')
+
+    @books = Art.where(category: 'book')
+
+    @movies = Art.where(category: 'movie')
+  end
+end
