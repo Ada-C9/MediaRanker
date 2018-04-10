ff674efb64bdd003c189bc5934b99c30647ee70f
Anne <annehwatson@gmail.com>
2018-04-10 09:25:35 -0700

    first test passing on work create method

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,5 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :work
 
-
+  #how to validate the combo of work_id and user_id?
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,18 @@
 require "test_helper"
 
 describe Work do
-  let(:work) { Work.new }
+  describe "validations" do
+    before do
+      @work = Work.new(category: "album", title: "test album", creator: "Anne", publication_year: 2018, description: "best album")
+    end
 
-  it "must be valid" do
-    value(work).must_be :valid?
+    it "can be created" do
+      result = @work.valid?
+      result.must_equal true
+    end
+  end
+
+  describe "relations" do
+
   end
 end
