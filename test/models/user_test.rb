505f7a53aede1b46b1e581e7ad3655f82407bfb4
Brenda Rios <riosbrenda@gmail.com>
2018-04-10 16:05:08 -0700

    Tests for users are completed and passing.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,12 +3,12 @@ require "test_helper"
 describe User do
   describe "validations" do
     it "is valid with a name" do
-      u = users(:cheetara)
+      u = users(:yellowlion)
       result = u.valid?
       result.must_equal true
     end
 
-    it "must have a name" do
+    it "is invalid without a name" do
       u = User.new
       result = u.valid?
       result.must_equal false
@@ -18,7 +18,12 @@ describe User do
 
   describe "relations" do
     it "must have votes" do
-      u = users(:cheetara)
+      u = users(:yellowlion)
+      u.votes.count.must_equal 2
+    end
+
+    it "must have votes" do
+      u = users(:littlepony)
       u.votes.count.must_equal 0
     end
   end
