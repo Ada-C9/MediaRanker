b85146825b2e0da587f9e45c8c2e98c88de452e8
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-04-09 23:50:09 -0700

    Welcome page controller and index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    @movies = Work.all.where(category: "movie").limit(10)
+    @books = Work.all.where(category: "book").limit(10)
+    @albums = Work.all.where(category: "album").limit(10)
   end
 end
