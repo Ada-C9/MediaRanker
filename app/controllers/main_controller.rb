0568fd1625b089236fb6d2951257e88af8b83cb0
Brenda Rios <riosbrenda@gmail.com>
2018-04-10 15:04:23 -0700

    Created class methods in work model to add logic of top-ten per category.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,11 +1,16 @@
 class MainController < ApplicationController
 
   def index
+    # @media_spotlight = Work.first
+    #
+    # @top_movies = Work.where(category: "movie").limit(10)
+    # @top_albums = Work.where(category: "album").limit(10)
+    # @top_books = Work.where(category: "book").limit(10)
+    
     @media_spotlight = Work.first
 
-    @top_movies = Work.where(category: "movie").limit(10)
-    @top_albums = Work.where(category: "album").limit(10)
-    @top_books = Work.where(category: "book").limit(10)
-
+    @top_ten_movies
+    @top_ten_albums
+    @top_ten_books
   end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -2,9 +2,29 @@ class Work < ApplicationRecord
 
   has_many :votes
 
-  validates :category, presence: {message: "Select a category"}
+  # validates :category, presence: {message: "Select a category"}
   validates :title, presence: {message: "Enter a title"}
-  validates :creator,presence: {message: "Enter a creator"}
-  validates :publication_year, presence: {message: "Enter publication_year year"}
-  
+  # validates :creator,presence: {message: "Enter a creator"}
+  # validates :publication_year, presence: {message: "Enter publication_year year"}
+  def self.media_spotlight
+    works = Work.all
+    works.max_by do |work|
+      work.votes.count
+    end
+  end
+
+  def self.top_ten_albums
+  album = Work.where(category: "album")
+  return album.sort_by {|work| -work.votes.count }.take(10)
+  end
+
+  def self.top_ten_movies
+  movie = Work.where(category: "movie")
+  return movie.sort_by {|work| -work.votes.count }.take(10)
+  end
+
+  def self.top_ten_books
+  book = Work.where(category: "book")
+  return book.sort_by {|work| -work.votes.count }.take(10)
+  end
 end
