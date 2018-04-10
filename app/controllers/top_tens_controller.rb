82f9e40fd4b756a4b78b6b19b0e435f4605946f0
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-04-10 14:49:01 -0700

    Define logic for displaying top ten works in descending order by vote count

--- app/controllers/top_tens_controller.rb
+++ app/controllers/top_tens_controller.rb
@@ -1,4 +1,14 @@
 class TopTensController < ApplicationController
   def index
-  end 
+    works = Work.all
+
+    # order works by vote count
+    movies = works.where(category: "movie")
+    books = works.where(category: "book")
+    albums = works.where(category: "album")
+
+    @top_movies = (movies[0..9].sort_by { |work| work.votes.count }).reverse!
+    @top_books = (books[0..9].sort_by { |work| work.votes.count }).reverse!
+    @top_albums = (albums[0..9].sort_by { |work| work.votes.count }).reverse!
+  end
 end
