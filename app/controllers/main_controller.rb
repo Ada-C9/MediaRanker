0df885784f15ce5554376bed3ba4b8e2b50fd96e
Brenda Rios <riosbrenda@gmail.com>
2018-04-10 14:39:37 -0700

    displayed top media main page.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,5 +6,6 @@ class MainController < ApplicationController
     @top_movies = Work.where(category: "movie").limit(10)
     @top_albums = Work.where(category: "album").limit(10)
     @top_books = Work.where(category: "book").limit(10)
+
   end
 end
