47704f7492e36d3319085941663503b8ded6b569
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-04-10 16:41:19 -0700

    Add database and few tests for work.valid?, all tests passing

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -2,8 +2,17 @@ require "test_helper"
 
 describe Work do
   let(:work) { Work.new }
+  let(:book) {works(:hp)}
+  let(:album) {works(:imagine)}
+  let(:movie) {works(:coco)}
 
-  it "must be valid" do
-    value(work).must_be :valid?
+  it "has validation for empty parameters" do
+    work.valid?.must_equal false
   end
+  it "returns true if valid?" do
+    book.valid?.must_equal true
+    album.valid?.must_equal true
+    book.valid?.must_equal true
+  end
+
 end
