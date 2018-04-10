a205a067bbac95c226b8826f8efb0569a5217afa
Steffany Brown <steffannbrown@gmail.com>
2018-04-10 16:30:50 -0700

    added more tests to publication_test

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,7 +1,9 @@
 class Publication < ApplicationRecord
   has_many :votes
+  validates :title, presence: true, length: {minimum: 1, maximum: 30}
 
-  validates :publication_year, length: { is: 4 }
-  validates :publication_year, presence: true
+  validates :creator, presence: true, length:  {minimum: 1, maximum: 25}
+
+  validates :publication_year, presence: true, length: { is: 4 }
   validates :publication_year, numericality: { only_integer: true, less_than_or_equal_to: 2018 }
 end
--- test/models/publication_test.rb
+++ test/models/publication_test.rb
@@ -4,17 +4,8 @@ describe Publication do
   let(:publication) { Publication.new }
 
   describe 'publication' do
-    it "has a creator" do
-      publication = publications(:reader)
-      publication.creator.must_equal "writer"
-    end
 
-    it "has a valid year" do
-      publication = publications(:reader)
-      publication.publication_year.must_equal 1991
-    end
-
-    it "does not accept years that are too short" do
+      it "does not accept years that are too short" do
       publication = Publication.new(publication_year: 123)
       publication.valid?.must_equal false
       publication.errors.must_include :publication_year
@@ -24,6 +15,48 @@ describe Publication do
       publication = Publication.new(publication_year: 12344)
       publication.valid?.must_equal false
       publication.errors.must_include :publication_year
+    end
+
+    it "requires title" do
+      publication = Publication.new(title: "")
+      publication.valid?.must_equal false
+      publication.errors.must_include :title
+    end
+
+    it "does not accept a too short title" do
+      publication = Publication.new(title:"whateverwhateverwhateverwhateverwhatever")
+      publication.valid?.must_equal false
+      publication.errors.must_include :title
+    end
+
+    it "does not accept a too long title" do
+      publication = Publication.new
+      publication.valid?.must_equal false
+      publication.errors.must_include :title
+    end
+
+    it "requires creator" do
+      publication = Publication.new
+      publication.valid?.must_equal false
+      publication.errors.must_include :creator
+    end
+
+    it "requires publication year" do
+      publication = Publication.new
+      publication.valid?.must_equal false
+      publication.errors.must_include :publication_year
+    end
+
+    it "does not accept a too short creator" do
+      publication = Publication.new(creator:"whateverwhateverwhateverwhateverwhatever")
+      publication.valid?.must_equal false
+      publication.errors.must_include :creator
+    end
+
+    it "does not accept a too long creator" do
+      publication = Publication.new(creator: "")
+      publication.valid?.must_equal false
+      publication.errors.must_include :creator
     end
   end
 end
