49b94ccc8137dd4c2bbb7a472dab528d7827aa55
Madaleine Shields <maddiev.shields@gmail.com>
2018-04-09 17:32:16 -0700

    Vote controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,22 @@
 class VotesController < ApplicationController
   def index
+    @votes = Vote.all
+  end
+
+  def show
+    id = params[:id]
+    @vote = Vote.find(id)
   end
 
   def new
+    @vote = Vote.new
   end
+
+  def create
+    @vote = Vote.new(vote_params)
+    if @vote.save
+      redirect_to votes_path
+    else
+      render :new
+  end 
 end
