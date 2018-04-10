2e8e00262a1073456b8e29bcb9f11772406f3b6e
Kaitlin Forsman <kcforsman@outlook.com>
2018-04-09 17:03:47 -0700

    add routes for homepage and publications

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,8 @@
 Rails.application.routes.draw do
-  get 'publications/index'
 
-  get 'publications/show'
+  resources :homepage, only: [:index]
 
-  get 'publications/new'
-
-  get 'publications/create'
-
-  get 'publications/edit'
-
-  get 'publications/update'
-
-  get 'publications/destroy'
-
-  get 'homepage/index'
+  resources :publications
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
