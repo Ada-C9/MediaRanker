3d31858c9cc7d2bd311e1a2989302e9119bc1f77
Steffany Brown <steffannbrown@gmail.com>
2018-04-10 16:14:32 -0700

    added publication validations to model and began publication_test

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,3 +1,7 @@
 class Publication < ApplicationRecord
   has_many :votes
+
+  validates :publication_year, length: { is: 4 }
+  validates :publication_year, presence: true
+  validates :publication_year, numericality: { only_integer: true, less_than_or_equal_to: 2018 }
 end
--- test/models/publication_test.rb
+++ test/models/publication_test.rb
@@ -3,7 +3,27 @@ require "test_helper"
 describe Publication do
   let(:publication) { Publication.new }
 
-  it "must be valid" do
-    value(publication).must_be :valid?
+  describe 'publication' do
+    it "has a creator" do
+      publication = publications(:reader)
+      publication.creator.must_equal "writer"
+    end
+
+    it "has a valid year" do
+      publication = publications(:reader)
+      publication.publication_year.must_equal 1991
+    end
+
+    it "does not accept years that are too short" do
+      publication = Publication.new(publication_year: 123)
+      publication.valid?.must_equal false
+      publication.errors.must_include :publication_year
+    end
+
+    it "does not accept years that are too long" do
+      publication = Publication.new(publication_year: 12344)
+      publication.valid?.must_equal false
+      publication.errors.must_include :publication_year
+    end
   end
 end
