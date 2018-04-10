d3900cfaf27e8a73e25bc4aea5cc1de54d7391a1
Ari <dominiquearia0@gmail.com>
2018-04-10 12:52:54 -0700

    Fixed data type for publication year, added foundation

---
+++ db/migrate/20180410193844_change_data_type_of_publication_year.rb
@@ -0,0 +1,5 @@
+class ChangeDataTypeOfPublicationYear < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :works, :publication_year
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409235604) do
+ActiveRecord::Schema.define(version: 20180410194745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,11 +18,11 @@ ActiveRecord::Schema.define(version: 20180409235604) do
   create_table "works", force: :cascade do |t|
     t.string "title"
     t.string "creator"
-    t.date "publication_year"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "category"
+    t.integer "publication_year"
   end
 
 end
