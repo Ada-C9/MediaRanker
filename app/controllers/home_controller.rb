2a8f9675e870d598146855a433a50875fb3f31ab
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-04-10 16:28:09 -0700

    User new page and show page in progress

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
-  include HomeHelper
 
   def index
     @spotlight = Work.media_spotlight
+
     @top_m = Work.top_movies
     @top_b = Work.top_books
     @top_a = Work.top_albums
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
+  resources :users, :only => [:index, :show, :new, :create]
   resources :works
 end
