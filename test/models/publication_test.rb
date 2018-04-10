5d7a48300459b233d636f9e9d1c1ed25ddfe0273
Kaitlin Forsman <kcforsman@outlook.com>
2018-04-10 13:00:43 -0700

    stubbed out initial tests for publication

--- test/models/publication_test.rb
+++ test/models/publication_test.rb
@@ -3,7 +3,30 @@ require "test_helper"
 describe Publication do
   let(:publication) { Publication.new }
 
-  it "must be valid" do
-    value(publication).must_be :valid?
+  describe 'publication validity'
+    it 'must be valid' do
+    end
+    it 'requires a title and category' do
+    end
+    it 'wont a take an empty string for a title' do
+    end
+    it 'wont make a duplicate record' do
+    end
+  end
+
+  describe 'publication relationships'
+    it 'can have zero or more votes' do
+    end
+  end
+
+  describe 'custom methods' do
+    describe 'self.find_spotlight_publication' do
+    end
+    describe 'self.return_all_categories' do
+    end
+    describe 'self.find_top_ten_by' do
+    end
+    describe 'find_total_votes' do
+    end
   end
 end
