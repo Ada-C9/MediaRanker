c520433e0ae12cce23a1eb03a7dd7d2756c6024d
Kat <kat.eastman@gmail.com>
2018-04-10 15:39:44 -0700

    added votes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   root 'works#root'
 
   resources :works
+  resources :users, only: [:show, :index]
 
   # get '/signup', to:
   # post '/signup', to:
 
   get '/login', to: 'sessions#new', as: :login
-  post '/login', to: 'sessions#create'
+  post '/login', to: 'sessions#login'
+  delete '/logout', to: 'sessions#logout', as: :logout
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
