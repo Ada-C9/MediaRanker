f82b20afda6bf7d194a39363f1b35c4d4c6f6e6f
Dikla <dikla.aharoni@gmail.com>
2018-04-09 23:27:58 -0700

    Added: back to media renks, edit and delete links to work show page

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,5 +1,5 @@
 class WorksController < ApplicationController
-  
+
   def index
     @works = Work.all
   end
@@ -9,6 +9,13 @@ class WorksController < ApplicationController
   end
 
   def create
+    @work = Work.new(work_params)
+
+    if @work.save
+      redirect_to works_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -16,11 +23,27 @@ class WorksController < ApplicationController
   end
 
   def update
+    @work = Work.find(params[:id])
+    @work.assign_atrributes(work_params)
+    if @work.save
+      #  ?
+      redirect_to work_path(work)
+    else
+      render :edit
+    end
   end
 
   def edit
+    @work = Work.find(params[:id])
   end
 
   def destroy
+    Work.destroy(params[:id])
+    redirect_to works_path
+  end
+
+  private
+  def work_params
+    params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
   end
 end
