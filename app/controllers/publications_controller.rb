da8ba49ac8b256fa7f3e5209e94e1e2e69785571
Kaitlin Forsman <kcforsman@outlook.com>
2018-04-09 17:19:29 -0700

    began filling out controller methods for homepage and publications

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,22 +1,50 @@
 class PublicationsController < ApplicationController
   def index
+    @publications = Publication.all.order(:category)
   end
 
   def show
+    @publication = Publication.find(params[:id])
   end
 
   def new
+    @publication = Publication.new
   end
 
   def create
+    @publication = Publication.new(publication_params)
+    if @publication.save
+      redirect_to publication_path(@publication.id)
+    else
+      render :new
+    end
   end
 
   def edit
+    @publication = Publication.find(params[:id])
   end
 
   def update
+    @publication = Publication.find(params[:id])
+    if @publication.update(publication_params)
+      redirect_to publication_path(@publication.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @publication = Publication.find(params[:id])
+    if @publication.destroy
+      redirect_to publications_path
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def publication_params
+    return params.require(:book).permit(:category, :title, :creator, :publication_year, :description)
   end
 end
