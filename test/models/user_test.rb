7740bc38e8a1a8240cd85f6d16e257fc7b74079d
Brenda Rios <riosbrenda@gmail.com>
2018-04-09 21:58:47 -0700

    Created fixtures in user yml and tests for user updated.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,10 +1,9 @@
 require "test_helper"
 
 describe User do
-  
-  describe 'validations' do
+  describe "validations" do
     it "is valid with a name" do
-      u = User.new(name: "bob")
+      u = users(:cheetara)
       result = u.valid?
       result.must_equal true
     end
@@ -13,14 +12,14 @@ describe User do
       u = User.new
       result = u.valid?
       result.must_equal false
+      u.errors.messages.must_include :name
     end
   end
 
-  describe 'relationships' do
+  describe "relations" do
     it "must have votes" do
-      u = User.new(name: "bob")
+      u = users(:cheetara)
       u.votes.count.must_equal 0
     end
-
   end
 end
