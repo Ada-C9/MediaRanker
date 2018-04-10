faec215743bb0c573f4c8d84caf47003f1cefb76
Maja Graonic <m.graonic@gmail.com>
2018-04-10 16:51:59 -0700

    generated controllers and models for users and votes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
 get '/works', to: 'works#index'
 get '/works/:id', to: 'works#show', as: 'work'
+
+
 
 end
