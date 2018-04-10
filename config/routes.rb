5e64eaf852c7e3ce045366bbd9c7bcc0ee803931
Phoebe <phoebe.manning8@gmail.com>
2018-04-10 15:07:22 -0700

    creates work controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'works/index'
+
+  get 'works/new'
+
+  get 'works/create'
+
+  get 'works/edit'
+
+  get 'works/update'
+
+  get 'works/show'
+
+  get 'works/destroy'
+
+  get 'works/upvote'
+
   root 'pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
