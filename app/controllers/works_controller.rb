8c04d03454b04f5993b30ff411edf10d0b3bbbc9
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-04-09 19:15:14 -0700

    New and create functions fully working

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -13,9 +13,9 @@ class WorksController < ApplicationController
   end
 
   def create
-    @work
+    work = Work.new(work_params)
 
-    if @work.save
+    if work.save
       redirect_to works_path
     else
       render :new
