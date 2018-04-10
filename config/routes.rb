691bc73b8dd321b1b5fbe7c6e6c6752f310ba3f8
Karinna Iniguez <karinnainiguez@gmail.com>
2018-04-09 21:29:57 -0700

    Add tests for user model and make them pass with validation

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
 
   resources :works
 
+  resources :users
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,29 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
+  describe "validations" do
 
-  it "must be valid" do
-    value(user).must_be :valid?
+    it "can be created with all attributes" do
+      user = User.new(username: "TestUser")
+
+      result = user.valid?
+
+      result.must_equal true
+    end
+
+    it "is invalid without a username" do
+      user = User.new
+
+      result = user.valid?
+
+      result.must_equal false
+    end
+
+
   end
+
+  describe "relations" do
+
+  end
+
 end
