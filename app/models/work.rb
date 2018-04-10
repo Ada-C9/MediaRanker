438c715640a8fc1f0579ec9e06275f4e3f762f8b
Mariko <marikoanderson@gmail.com>
2018-04-09 17:42:16 -0700

    added validations for user and work

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,8 @@
 class Work < ApplicationRecord
   has_many :votes
+
+  validates :category, pressence: {message: "Must provide a category"}
+
+  validates :title, pressence: {message: "Please provide a title"}
+  validates :title, uniqueness: {message: "Work already exists" }
 end
