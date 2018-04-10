c3b0574c329b881414fe76307965433ff33afd28
AngelaPoland <angelalpoland@gmail.com>
2018-04-10 16:58:17 -0700

    added some css for welcome page columns, put works index sections into tables

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,15 +1,21 @@
 class WorksController < ApplicationController
   def index
     @works = Work.all
+    # @movies = Work.where(category: "movie")
+    # @books = Work.where(category: "book")
+    # @albums = Work.where(category: "album")
   end
 
   def new
   end
 
   def show
+    id = params[:id]
+    @work = Work.find(id)
   end
 
   def edit
+    @work = Work.find_by(id: params[:id])
   end
 
   def create
@@ -19,5 +25,9 @@ class WorksController < ApplicationController
   end
 
   def destroy
+    id = params[:id]
+    @work = Work.find(id)
+    @work.destroy
+    redirect_to works_path
   end
 end
