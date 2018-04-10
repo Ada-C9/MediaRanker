c737660658b119b10d14ed23cb197e5624fb4180
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-04-10 14:59:58 -0700

    Set up Works controller actions

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,27 @@
 class WorksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def works_params
+    return params.require(:work).permit(:title, :creator, :publication_year, :category, :description)
+  end
 end
