c4c37e879037ddd1a9cad087307697806905342f
Samantha Berk <sberk10@gmail.com>
2018-04-10 15:36:34 -0700

    remove redundancy in works model

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,7 +1,7 @@
 class Work < ApplicationRecord
   has_many :votes
 
-  validates :category, presence: true, uniqueness: { scope: :title}
+  validates :category, presence: true
   validates :title, presence: true, uniqueness: { scope: :category }
   validates :creator, presence: true
   validates :publication_year, presence: true
