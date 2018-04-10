dcc7e1b21e8ca434e195e6da03c26a01abb99e6d
Samantha Berk <sberk10@gmail.com>
2018-04-10 16:08:42 -0700

    Create basic structure for welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    @spotlight = Work.first
+    @movies = Work.where(category: 'movie')
+    @books = Work.where(category: 'book')
+    @albums = Work.where(category: 'album')
   end
 end
