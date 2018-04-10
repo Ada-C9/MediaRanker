8d2c3a48a3b375d7d49bc8e6a62798d794b65b7b
Hannah Cameron <hannahiscoding@gmail.com>
2018-04-10 15:04:37 -0700

    fixed typo in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   end
 
   get '/login', to: 'sessions#new', as: 'login'
-  post '/login', to 'sessions#create'
+  post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: 'logout'
 
 end
