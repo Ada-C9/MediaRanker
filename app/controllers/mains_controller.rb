67a060049589fa4396e1aacbc5781c9e968cde6e
Zheng Cao <chori84@gmail.com>
2018-04-09 19:21:22 -0700

    Show top 10 media of each category on home page

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -1,7 +1,7 @@
 class MainsController < ApplicationController
   def index
-    @albums = Work.all.where(category: "album").limit(2)
-    @books = Work.all.where(category: "book").limit(2)
-    @movies = Work.all.where(category: "movie").limit(2)
+    @albums = Work.all.where(category: "album").limit(10)
+    @books = Work.all.where(category: "book").limit(10)
+    @movies = Work.all.where(category: "movie").limit(10)
   end
 end
