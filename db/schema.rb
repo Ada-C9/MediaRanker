55c18b9c34014fad85743fd7560fb9aa4f890cdb
Karinna Iniguez <karinnainiguez@gmail.com>
2018-04-09 21:21:59 -0700

    Create model and controller for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409215814) do
+ActiveRecord::Schema.define(version: 20180410042031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
