16fbf9b137d97aaf9d41a2dae4b864d8cd7a6360
Zheng Cao <chori84@gmail.com>
2018-04-10 15:25:50 -0700

    Update routes for Sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
 
   root 'mains#index'
 
+  get '/login', to: 'sessions#new', as: 'login_form'
+  post '/login', to: 'sessions#create', as: 'login'
+  delete '/login', to: 'sessions#destroy', as: 'logout'
+
   resources :mains, only: [:index]
 
   resources :works
 
   resources :users, only: [:index, :show]
-
-  resources :sessions, only: [:new, :create, :destroy]
 
 end
