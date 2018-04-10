037abe66f0743d79e775aa6375f5c6fce1ad6704
Aruna <aruna.thungathurthi@gmail.com>
2018-04-09 21:13:25 -0700

    added the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,4 @@
 Rails.application.routes.draw do
-  get 'works/index'
-
-  get 'works/new'
-
-  get 'works/create'
-
-  get 'works/show'
-
-  get 'works/edit'
-
-  get 'works/update'
-
-  get 'works/destroy'
-
-  get 'main/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :works
+  root "main#index"
 end
