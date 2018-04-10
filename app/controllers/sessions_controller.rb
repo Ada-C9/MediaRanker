850be8f07ced90c89a8ac24b498b4a7863cfc3b3
Zheng Cao <chori84@gmail.com>
2018-04-10 15:35:51 -0700

    Implement Sessions#create controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(name: params[:user][:name])
+    if @user
+      session[:user_id] = @user.id
+      flash[:notice] = "Successfully logged in as existing user #{@user.name}"
+    else
+      @user = User.create(name: params[:user][:name])
+      session[:user_id] = @user.id
+      flash[:notice] = "Successfully created new user #{@user.name} with ID #{@user.id}"
+    end
+    redirect_to root_path
   end
 
   def destroy
