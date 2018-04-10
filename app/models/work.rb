30e4d00c8a7ffde22196a9949b9f9ba6b7e29ac3
Phoebe <phoebe.manning8@gmail.com>
2018-04-10 14:48:16 -0700

    formats works on homepage to be readable

--- app/models/work.rb
+++ app/models/work.rb
@@ -4,18 +4,18 @@ class Work < ApplicationRecord
   has_many :users, through: :votes
 
   def self.make_category_hash
-    category_hash = {}
+    category_hash = Hash.new
     i = 0
     while i < Work.all.length
       if category_hash.has_key?(Work.all[i].category) == false
         category_hash[Work.all[i].category] = []
         category_hash[Work.all[i].category] << Work.all[i]
+        # binding.pry
       else
         category_hash[Work.all[i].category] << Work.all[i]
       end
       i += 1
     end
-
     return category_hash
   end
 end
