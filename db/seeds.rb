e6480afcabe8cd4631813e7604bd434870fd2c8e
Kiera <kiera.thomasson@gmail.com>
2018-04-09 17:03:18 -0700

    Added user routes, updated seed file and added data to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+MEDIA_FILE = Rails.root.join('db', 'media_seeds.csv')
+puts "Loading raw driver data from #{MEDIA_FILE}"
+
+work_failures = []
+
+CSV.foreach(MEDIA_FILE, :headers => true) do |row|
+  work = Work.new
+  work.category = row['category']
+  work.title = row['title']
+  work.created_by = row['creator']
+  work.published_year = row['publication_on_year']
+  work.description = row['description']
+
+  successful = work.save
+  if !successful
+    work_failures << work
+    puts "Failed to save work: #{work.inspect}"
+  else
+    puts "Created work: #{work.inspect}"
+  end
+end
