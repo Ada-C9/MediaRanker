0a112e8f3739a1f392121274a49abdd2dbbc0e97
AngelaPoland <angelalpoland@gmail.com>
2018-04-09 17:31:30 -0700

    added validations to work model and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_many :votes
+
+  validates_presence_of :user_name
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,5 @@
 class Work < ApplicationRecord
   has_many :votes
+
+  validates_presence_of :title, :category
 end
