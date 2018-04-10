dc5c55c769f37dc8be44cb807e2b7c5341a1ac71
Samantha Berk <sberk10@gmail.com>
2018-04-10 14:38:11 -0700

    Create tests for Works validations and validations in the model

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,9 @@
 class Work < ApplicationRecord
   has_many :votes
+
+  validates :category, presence: true
+  validates :title, presence: true, uniqueness: true
+  validates :creator, presence: true
+  validates :publication_year, presence: true
+
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,51 @@
 require "test_helper"
 
 describe Work do
-  let(:work) { Work.new }
 
-  it "must be valid" do
-    value(work).must_be :valid?
+  describe "validations" do
+
+    before do
+      @work = Work.new(category: 'album', title: 'test title', creator: 'test creator', publication_year: 'test year')
+    end
+
+    it "can be created with all required fields" do
+      result = @work.valid?
+
+      result.must_equal true
+    end
+
+    it "is invalid without a category" do
+      @work.category = nil
+
+      result = @work.valid?
+
+      result.must_equal false
+    end
+
+    it "is invalid without a title" do
+      @work.title = nil
+
+      result = @work.valid?
+
+      result.must_equal false
+    end
+
+    it "is invalid without a creator" do
+      @work.creator = nil
+
+      result = @work.valid?
+
+      result.must_equal false
+    end
+
+    it "is invalid without a publication year" do
+      @work.publication_year = nil
+
+      result = @work.valid?
+
+      result.must_equal false
+    end
+
   end
+
 end
