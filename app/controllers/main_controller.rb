ae8b4e8d7cec56f043b237cc15ef4914bc0a9616
Brenda Rios <riosbrenda@gmail.com>
2018-04-09 20:34:42 -0700

    Created main controller.

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,10 @@
+class MainController < ApplicationController
+
+  def index
+    @media_spotlight = Work.first
+
+    @top_movies = Work.where(category: "movie").limit(10)
+    @top_albums = Work.where(category: "album").limit(10)
+    @top_books = Work.where(category: "book").limit(10)
+  end
+end
