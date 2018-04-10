7629e84185642ee61dbc0424c0e968bd7a475728
Kaitlin Forsman <kcforsman@outlook.com>
2018-04-10 12:41:08 -0700

    went through content added so far and updated any missing dynamic content I could find, such as links to the publication show pages from each title name

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,10 +1,13 @@
 class Publication < ApplicationRecord
-  def return_all_categories
+  def self.find_spotlight_publication
+    Publication.first
   end
-  def find_top_ten_by category
+  def self.return_all_categories
+    media = ['album', 'book']
+  end
+  def self.find_top_ten_by category
+    Publication.first(10)
   end
   def find_total_votes
-  end
-  def find_spotlight_publication
   end
 end
