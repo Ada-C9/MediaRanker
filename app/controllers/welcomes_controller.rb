3cab620959eebcabaf1a4e2815c3084f79e6c374
Nora Peters <norapeters5@gmail.com>
2018-04-10 16:09:13 -0700

    adds movies to seed file. adds top ten lists for each work to homepage, from welcome#index

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,4 +1,7 @@
 class WelcomesController < ApplicationController
   def index
+    @movies = Work.where(category: 'movie').limit(10)
+    @books = Work.where(category: 'book').limit(10)
+    @albums = Work.where(category: 'album').limit(10)
   end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,5 @@
 class Work < ApplicationRecord
   has_many :votes
   validates_presence_of :title, :category
+
 end
