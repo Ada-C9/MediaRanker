20132e6b3d7cd8863fe66a468a8f4a11f9af5c0d
Brenda Rios <riosbrenda@gmail.com>
2018-04-09 20:51:55 -0700

    Added validations to vote and work models.

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,10 @@
 class Work < ApplicationRecord
+
+  has_many :votes
+
+  validates :category, presence: {message: "Select a category"}
+  validates :title, presence: {message: "Enter a title"}
+  validates :creator,presence: {message: "Enter a creator"}
+  validates :publication_year, presence: {message: "Enter publication_year year"}
+  
 end
