22f6f5370756781acbaf69e2d16624e67086c20b
Aruna <aruna.thungathurthi@gmail.com>
2018-04-09 20:43:26 -0700

    created main and work controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  get 'works/index'
+
+  get 'works/new'
+
+  get 'works/create'
+
+  get 'works/show'
+
+  get 'works/edit'
+
+  get 'works/update'
+
+  get 'works/destroy'
+
+  get 'main/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
