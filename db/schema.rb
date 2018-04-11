6e6d2c1181b6508fae4558c81b1bc0293db92da6
LetiTran <leticiastran@gmail.com>
2018-04-11 14:42:27 -0700

    Created migration to add work_id to votes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411213806) do
+ActiveRecord::Schema.define(version: 20180411214034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,9 @@ ActiveRecord::Schema.define(version: 20180411213806) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.bigint "work_id"
     t.index ["user_id"], name: "index_votes_on_user_id"
+    t.index ["work_id"], name: "index_votes_on_work_id"
   end
 
   create_table "works", force: :cascade do |t|
@@ -39,4 +41,5 @@ ActiveRecord::Schema.define(version: 20180411213806) do
   end
 
   add_foreign_key "votes", "users"
+  add_foreign_key "votes", "works"
 end
