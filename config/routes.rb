a64584d0173d873586d7385ee628275b3310e294
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-04-10 22:34:45 -0700

    Updated users route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   root to: 'welcome#index'
-  resources :works, :users
+  resources :works
+  resources :users, only: [:index, :show]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
