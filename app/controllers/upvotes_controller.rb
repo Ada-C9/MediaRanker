458f01c79cf42227b995d530ff21e4f8ba5e2564
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-04-11 15:37:32 -0700

    Add create method and provided a link to upvote

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -1,4 +1,18 @@
 class UpvotesController < ApplicationController
   def create
+    @upvote = Upvote.new(user_id:session[:user_id], work_id:params[:id])
+    if @upvote.save
+      flash[:success] = "Successfully upvoted!"
+      redirect_to works_path
+    else
+      error = @upvote.errors
+      if !error[:user].empty?
+        flash[:error] = "You must log in to do that"
+      else
+        flash[:error] = "You can't upvote more than once!"
+      end
+      redirect_to works_path
+    end
+
   end
 end
