f2616e7ea3c3641010279c4524b9c580ad6a8bd5
Abiaina <abinnet.a@gmail.com>
2018-04-10 22:32:26 -0700

    set basic show for work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_many :works, through :votes
+  has_many :votes
 
   validates :username, presence: true
   validates :username, length: { in: 2..25 }
 
-  validate :username uniqueness: true,
+  validate :username, uniqueness: true,
 end
