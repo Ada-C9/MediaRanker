54c86e7f4d2993ca9603df15b8f3196978d34068
Samantha Berk <sberk10@gmail.com>
2018-04-11 15:13:36 -0700

    Add #logout to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,4 +17,12 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
+  def logout
+    session.delete(:user_id)
+    flash[:success] = "Logged out successfully"
+
+    redirect_to root_path
+
+  end
+
 end
