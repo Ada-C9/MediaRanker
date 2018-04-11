ce8ee6c1454ca4cb8a43402f08cca62c8f535bf0
Phoebe <phoebe.manning8@gmail.com>
2018-04-10 18:38:09 -0700

    adds category presence validation, and custom method tests

--- app/models/work.rb
+++ app/models/work.rb
@@ -3,7 +3,7 @@ class Work < ApplicationRecord
   has_many :users, through: :votes
 
   validates :title, presence: true, uniqueness: true
-  validates :creator, presence: true
+  validates :creator, :category, presence: true
   validates :published, format: { with: /\d{4}/ }, numericality: { less_than_or_equal_to: Time.now.strftime("%Y").to_i }
 
   def self.make_category_hash
@@ -18,13 +18,15 @@ class Work < ApplicationRecord
     return categories
   end
 
+  def self.spotlight
+
+  end
+
+  private
   def self.num_runs(array)
     runs = 0
     array.length > 10 ? runs = 10 : runs = array.length
     return runs
   end
 
-  def self.spotlight
-
-  end
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 describe Work do
   let(:work1) { works(:work1) }
   let(:work2) { works(:work2) }
-  let(:dup_work) { works(:dup_work) }
+  let(:all_works) { Work.all }
 
   describe "validations" do
     it "is valid when all fields are present" do
@@ -11,13 +11,15 @@ describe Work do
       result.must_equal true
     end
 
-    it "is not valid when required fields are no present" do
+    it "is not valid when required fields are not present" do
       work2.title = nil
       work2.creator = nil
+      work2.category = nil
       result = work2.valid?
       result.must_equal false
       work2.errors.messages.must_include :title
       work2.errors.messages.must_include :creator
+      work2.errors.messages.must_include :category
     end
 
     it "is not valid when work titles are not unique" do
@@ -37,6 +39,31 @@ describe Work do
       work1.published = 1
       work1.valid?.must_equal false
     end
+  end
 
+  describe "relations" do
+
+  end
+
+  describe "custom methods" do
+    describe "#make_category_hash" do
+      it "returns an array" do
+        all_works.make_category_hash.must_be_kind_of Array
+      end
+
+      it "returns correct array for valid data" do
+        all_works.make_category_hash.must_include "book"
+        all_works.make_category_hash.must_include "album"
+        all_works.make_category_hash.must_include "movie"
+        all_works.make_category_hash.count.must_equal 3
+      end
+
+      it "returns empty array if there are no works" do
+        all_works.each do |work|
+          work.destroy
+        end
+        all_works.make_category_hash.must_equal []
+      end
+    end
   end
 end
