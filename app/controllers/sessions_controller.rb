e316ac3875d33e0d61cb362d770649fa42b0a0ff
Brenda Rios <riosbrenda@gmail.com>
2018-04-11 14:47:00 -0700

    Created sessions controller.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,33 @@
+class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    name = params[:user][:name]
+    user = User.find_by(name: name)
+
+    if user
+      session[:logged_in_user] = user.id
+      flash[:success] = "#{ user.name } is successfully logged in"
+      redirect_to root_path
+    else
+      user = User.new(name: name, created_at: Date.today)
+      user.save
+      flash[:status] = :success
+      flash[:message] = "Successfully created new user #{name} with ID #{user.id}"
+      session[:logged_in_user] = user.id
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    session[:logged_in_user] = nil
+    flash[:status] = :success
+    flash[:message] = "Successfully logged out"
+    redirect_to root_path
+  end
+
+
+end
