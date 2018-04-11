7bc09f30095520e180a1db28541eb645e6668c56
Steffany Brown <steffannbrown@gmail.com>
2018-04-10 17:15:41 -0700

    added new and create to publication model, updated new view

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -3,20 +3,24 @@ class PublicationsController < ApplicationController
     @albums = Publication.where(category: "album")
     @books = Publication.where(category: "book")
     @movies = Publication.where(category: "movie")
-    @spotlight = Publication.first
   end
 
   def show
+    @publication = Publication.find(params[:id])
 
   end
 
   def create
+    @publication = Publication.new(publication_params)
+    @publication.save ? (redirect_to publication_path(@publication[:id])) : (render :new)
   end
 
   def destroy
   end
 
   def new
+    @publication = Publication.new
+
   end
 
   def edit
@@ -24,4 +28,10 @@ class PublicationsController < ApplicationController
 
   def update
   end
+
+  private
+
+  def publication_params
+  return params.require(:publication).permit(:title, :creator, :category, :publication_year, :description)
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :users
   resources :publications
 
-  root 'mains#index'
+  root 'mains#index', as: 'main'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
