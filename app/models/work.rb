ea48b08cf3dded09190f4e15c7ac18e49efcf9cb
Zheng Cao <chori84@gmail.com>
2018-04-10 20:04:30 -0700

    Add vote fixtures, update work model, and implement Work validations and relations tests

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,4 @@
 class Work < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: { scope: :category }
   has_many :votes, dependent: :destroy
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -2,15 +2,54 @@ require "test_helper"
 
 describe Work do
 
-  it "is valid when all fields are present" do
-    works(:aja).valid?.must_equal true
+  describe 'Validations' do
+    # nominal
+    it "is valid when all fields are present" do
+      works(:aja).valid?.must_equal true
+    end
+
+    # presence
+    it "is invalid without a title" do
+      works(:aja).title = nil
+
+      works(:aja).valid?.must_equal false
+      works(:aja).errors.messages.must_include :title
+    end
+
+    it "is invalid if the title is empty string" do
+      works(:aja).title = ""
+
+      works(:aja).valid?.must_equal false
+      works(:aja).errors.messages.must_include :title
+    end
+
+    # uniqueness
+    it "is invalid if a work with same title and category already exists" do
+      work = Work.new(title: works(:aja).title, category: works(:aja).category)
+
+      work.valid?.must_equal false
+      work.errors.messages.must_include :title
+    end
   end
 
-  it "is invalid without a title" do
-    works(:aja).title = nil
+  describe 'Relations' do
+    it "can access votes through 'votes'" do
+      works(:aja).votes.count.must_equal 1
+    end
 
-    works(:aja).valid?.must_equal false
-    works(:aja).errors.messages.must_include :title
+    it "can destroy votes through Work destroy action" do
+      votes = works(:aja).votes
+
+      votes.wont_equal []
+
+      works(:aja).destroy
+
+      votes.must_equal []
+    end
+  end
+
+  describe 'Methods' do
+
   end
 
 end
