ad7f717e4731a8aa6d130e7a156659cf2af36dba
Brittany Jones <brittany.r.jones@hotmail.com>
2018-04-11 14:30:31 -0700

    added some methods, created table for show works, added sessions controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
 
 
   def index
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
   def show
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -39,14 +39,14 @@ class UsersController < ApplicationController
   def logout
   end
 
-  def find_user
-    @user = User.find(params[:id])
-  end
 
   private
   def user_params
     params.require(:user).permit(:name)
   end
 
+  def find_user
+    @user = User.find(params[:id])
+  end
 
 end
