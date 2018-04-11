2da4842588ae61154f47fa16c6936e5c38ea88f3
marikoja <Marikoanderson@gmail.com>
2018-04-11 16:04:19 -0700

    GAHHHH SPELLING who needs it? oh me if I want to use heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :votes
 
-  validates :user_name, uniquuness: {message: "User Name already in use, please enter another User Name."}
+  validates :user_name, uniqueness: {message: "User Name already in use, please enter another User Name."}
 
   validates :user_name, presence: { message: "You need to provide a User Name" }
 end
