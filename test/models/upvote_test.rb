8dbf84efc6b623e785b5167f7205cad195fb39c9
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-04-11 15:52:13 -0700

    Add test database for upvote and some tests

--- test/models/upvote_test.rb
+++ test/models/upvote_test.rb
@@ -2,8 +2,23 @@ require "test_helper"
 
 describe Upvote do
   let(:upvote) { Upvote.new }
+  let(:vote) { upvotes(:one) }
 
-  it "must be valid" do
-    value(upvote).must_be :valid?
+
+  it "has validation for empty parameters" do
+    upvote.valid?.must_equal false
   end
+
+  it "has validation for both ids" do
+    vote.valid?.must_equal true
+  end
+
+  it "checks for validation with missing id" do
+  vote.work_id = nil
+  vote.valid?.must_equal false
+  vote.user_id = nil
+  vote.valid?.must_equal false
+end
+
+
 end
