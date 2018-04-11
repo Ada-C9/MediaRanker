0693de28a54b999a0794d050cd1c4d0f6d27b0d2
Zheng Cao <chori84@gmail.com>
2018-04-10 20:28:53 -0700

    Add User fixtures, update User model, and implement User validations and relations tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,55 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
 
-  it "must be valid" do
-    value(user).must_be :valid?
+  describe 'Validations' do
+    # nominal
+    it "is valid when all fields are present" do
+      users(:user_1).valid?.must_equal true
+    end
+
+    # presence
+    it "is invalid without a name" do
+      users(:user_1).name = nil
+
+      users(:user_1).valid?.must_equal false
+      users(:user_1).errors.messages.must_include :name
+    end
+
+    it "is invalid if name is a string with only spaces" do
+      users(:user_1).name = "    "
+
+      users(:user_1).valid?.must_equal false
+      users(:user_1).errors.messages.must_include :name
+    end
+
+    # uniqueness
+    it "is invalid if an user with same name already exists" do
+      user = User.new(name: users(:user_1).name)
+
+      user.valid?.must_equal false
+      user.errors.messages.must_include :name
+    end
   end
+
+  describe 'Relations' do
+    it "can access votes through 'votes'" do
+      users(:user_1).votes.count.must_equal 1
+    end
+
+    it "can destroy votes through destroying user" do
+      votes = users(:user_1).votes
+
+      votes.wont_equal []
+
+      users(:user_1).destroy
+
+      votes.must_equal []
+    end
+  end
+
+  describe 'Methods' do
+
+  end
+
 end
