3c5e09931c9c769783f5d2fb23790fcf02ec05bb
Samantha Berk <sberk10@gmail.com>
2018-04-11 12:28:45 -0700

    Create index action and view to show all users with appropriately formatted joined date

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
   has_many :votes
 
   validates :name, presence: true, uniqueness: true
+
+  def created_date
+    raw_date = self.created_at
+    stripped_date = raw_date.strftime("%B %d, %Y")
+    return stripped_date
+  end
+
 end
