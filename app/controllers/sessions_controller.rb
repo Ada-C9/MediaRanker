32f5c6277dad4341f3477e9fd0336034cc3b9bfd
LetiTran <leticiastran@gmail.com>
2018-04-11 11:44:23 -0700

    Created a new sessions_controller and added code for creating a new user, logging-in and added routes for it.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def create
+    @user = User.find_by name: params[:user][:name]
+    if @user
+      session[:user_id] = @user.id
+      flash[:sucess] = "Welcome back #{@user.name}"
+    else
+      @user = User.create name: params[:user][:name]
+      session[:user_id] = @user.id
+      flash[:sucess] = "Welcome #{@user.name}"
+    end
+    redirect_to root_path
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:sucess] = "You logged out!"
+    redirect_to root_path
+  end
+
+  def new
+    @user = User.new
+  end
+end
