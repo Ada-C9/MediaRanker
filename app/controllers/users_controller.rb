f0b292ab2d5e53317228e791312aab92b91ba442
Brittany Jones <brittany.r.jones@hotmail.com>
2018-04-11 15:05:36 -0700

    added user show page, needs formatting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,59 @@
 class UsersController < ApplicationController
-  def new
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @users = User.all
   end
 
-  def edit
-  end
-
-  def show
-  end
-
-  def update
-  end
-
-  def destroy
+  def show;
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+      flash[:success] = "Successfully created user #{@user.name}"
+      # session[:logged_in] = @user
+    else
+      render :new
+    end
   end
 
-  def index
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+     redirect_to user_path(@user.id)
+   else
+     render :edit
+   end
+ end
+
+
+
+  def login
+  end
+
+  def logout
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
 end
