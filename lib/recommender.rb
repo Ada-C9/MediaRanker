e0764ba53acbb54946a57aea22f17eb3efc4af12
Tor Shimizu <torshimizu@gmail.com>
2018-04-11 15:11:01 -0700

    starting file for recommender

---
+++ lib/recommender.rb
@@ -0,0 +1,16 @@
+class WorkRecommender
+
+  # a new instance of recommender for each user
+  def initialize(user)
+    @user = user
+  end
+
+  def self.recommendations
+
+  end
+
+  # parts needed:
+  # @user list of works
+  # 
+
+end
