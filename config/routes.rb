8065afddc38ececb615798e93d62a9983b6b887e
Maja Graonic <m.graonic@gmail.com>
2018-04-10 17:53:50 -0700

    replaced routes with route helpers, nested vote post route under works

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-get '/works', to: 'works#index'
-get '/works/:id', to: 'works#show', as: 'work'
+resources :works do
+  resources :votes, only: [:create]
+end
 
-
-
+resources :users, only: [:index, :show, :create]
 end
