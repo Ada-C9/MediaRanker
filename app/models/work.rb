1473e9fd6ee326be34c168029a4e3ccebb62a0b0
AngelaPoland <angelalpoland@gmail.com>
2018-04-10 17:51:48 -0700

    work controller vote_count method, added that method to work index page tables

--- app/models/work.rb
+++ app/models/work.rb
@@ -5,4 +5,8 @@ class Work < ApplicationRecord
 
   validates :title, uniqueness: { scope: :category, message: "Cannot have the same title within the same media type" }
 
+  def vote_count
+    self.votes.where(work_id: self.id).count
+  end
+
 end
