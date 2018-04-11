ecce98b0825041f5d242ebda871ebbef9d250dab
Zheng Cao <chori84@gmail.com>
2018-04-11 07:55:46 -0700

    Finish implementing SessionsController through controller filter

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
+  before_action :find_user, only: [:create, :destroy]
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.find_by(name: params[:user][:name])
     if @user
       session[:user_id] = @user.id
       flash[:notice] = "Successfully logged in as existing user #{@user.name}"
@@ -17,5 +18,15 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out"
+    redirect_to root_path
   end
+
+  private
+
+  def find_user
+    @user = User.find_by(name: params[:user][:name])
+  end
+
 end
