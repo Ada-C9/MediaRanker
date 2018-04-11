541dff94f4afa37f7b702a116ab11cebf80adb98
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-04-10 18:48:07 -0700

    Added a validation for categories

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,4 +2,5 @@ class Work < ApplicationRecord
   has_many :votes
   validates :title, presence: true
   validates :category, presence: true
+  validates :category, inclusion: { in: %w(movie book album)}
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -17,5 +17,18 @@ describe Work do
     value(work).wont_be :valid?
   end
 
+  it "must be valid with a supported category type" do
+    categories = ["movie", "book", "album"]
+    categories.each do |category|
+      work.category = category
+      value(work).must_be :valid?
+    end
+  end
+
+  it "must be invalid without a supported category type" do
+    work.category = "gdfhjgfhjd"
+    value(work).wont_be :valid?
+  end
+
 
 end
