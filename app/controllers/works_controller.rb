db7938155994e1138275d748b55cdc2a13ddd781
Kate Pond <kate@thepondsedge.com>
2018-04-11 15:47:21 -0700

    creates new and edit works views

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -9,22 +9,33 @@ class WorksController < ApplicationController
   end
 
   def new
+    @work = Work.new
   end
 
   def create
+    @work = Work.new(work_params)
   end
 
   def show
-
   end
 
   def edit
   end
 
+  def update
+    @work.update(work_params)
+  end
+
   def destroy
+    @work.destroy
+    redirect_to root_path
   end
 
   private
+
+  def work_params
+    return params.require(:work).permit(:title, :creator, :publication_date, :description, :category)
+  end
 
   def find_work
     @work = Work.find_by(id: params[:id])
