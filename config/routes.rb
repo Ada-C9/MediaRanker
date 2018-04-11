2f776554b73f775a081a503c6641320177d987e4
Kaitlin Forsman <kcforsman@outlook.com>
2018-04-11 14:28:08 -0700

    added users controller and content to user index and show views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+
   root 'homepage#index'
 
   resources :homepage, only: [:index]
 
   resources :publications
+
+  resources :users, only: [:index, :show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
