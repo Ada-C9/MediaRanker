176535d70c66c704d95042fe8a9a8343c31eaed7
Samantha Berk <sberk10@gmail.com>
2018-04-11 12:36:12 -0700

    Write tests for user validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,4 +2,34 @@ require "test_helper"
 
 describe User do
 
+  describe "validations" do
+
+    before do
+      @user = User.create!(name: 'test user')
+    end
+
+    it "can be created" do
+      result = @user.valid?
+
+      result.must_equal true
+    end
+
+    it "is invalid without a name" do
+      @user.name = nil
+
+      result = @user.valid?
+
+      result.must_equal false
+    end
+
+    it "cannot be created if another user with the same name exists" do
+      duplicate_user = User.new(name: 'test user')
+
+      result = duplicate_user.valid?
+
+      result.must_equal false
+    end
+
+  end
+
 end
