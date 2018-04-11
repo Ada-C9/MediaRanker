3859ded87e3d3ed7b65f1cb447c43e0d695553bb
Samantha Berk <sberk10@gmail.com>
2018-04-11 11:13:39 -0700

    Update syntax of controller filters in Works controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -20,11 +20,9 @@ class WorksController < ApplicationController
     end
   end
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
   def update
     @work.assign_attributes(work_params)
