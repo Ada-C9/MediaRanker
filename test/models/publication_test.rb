068d8aa5892dad818ad837459f52ce57122ab854
Kaitlin Forsman <kcforsman@outlook.com>
2018-04-11 00:08:15 -0700

    finished writing tests for current approach to model and the custom methods find_total_votes and find_top_ten_by

--- test/models/publication_test.rb
+++ test/models/publication_test.rb
@@ -2,8 +2,9 @@ require "test_helper"
 require 'date'
 
 describe Publication do
-  let(:publication) { Publication.new }
   let(:sailor_moon) { publications(:sailormoon) }
+  let(:top_ten_books) {Publication.find_top_ten_by("Book")}
+  let(:top_ten_movies) {Publication.find_top_ten_by("Movie")}
 
   describe 'publication validity' do
     it 'must be valid' do
@@ -74,26 +75,53 @@ describe Publication do
     end
     describe 'self.find_top_ten_by' do
       it 'returns an array of 10 publications for a category' do
+        top_ten_books.must_be_kind_of Array
+        top_ten_books.must_include publications(:sailormoon)
+        top_ten_books.must_include publications(:inuyasha)
+        top_ten_books.must_include publications(:hostclub)
+        top_ten_books.must_include publications(:bleach)
+        top_ten_books.must_include publications(:tsubasa)
+      end
+      it 'wont include records of other categories' do
+        top_ten_books.wont_include publications(:appleseed)
+        top_ten_books.wont_include publications(:cardcaptor)
       end
       it 'returns an array in descending order by number of votes per publication' do
+        top_ten_books.first.must_equal sailor_moon
+        top_ten_books.last.must_equal publications(:codenamev)
+        top_ten_books[1].must_equal publications(:fruits)
+        top_ten_books[2].must_equal publications(:bleach)
+        top_ten_books[3].must_equal publications(:hostclub)
       end
       it 'will not return more than 10 publications if more exist in a category' do
+        Publication.find_top_ten_by("TV Show").length.must_equal 10
+        Publication.find_top_ten_by("TV Show").wont_include publications(:sailorcrystal)
       end
       it 'returns no more publications than what exists in a category' do
+        top_ten_movies.length.must_equal 3
       end
       it 'returns the most recently voted on if equal votes for the 10th and 11th place' do
+        top_ten_books[4].must_equal publications(:dgray)
+        top_ten_books[5].must_equal publications(:tsubasa)
       end
       it 'returns the most recently created publications first if there are no votes' do
+        top_ten_movies.first.must_equal publications(:vampire)
+        top_ten_movies[1].must_equal publications(:appleseed)
+        top_ten_movies.last.must_equal publications(:spirited)
       end
       it 'returns error for a category that doesnt exist' do
+        proc { Publication.find_top_ten_by("Play") }.must_raise StandardError
       end
     end
     describe 'find_total_votes' do
       it 'returns an integer' do
+        sailor_moon.find_total_votes.must_be_kind_of Integer
       end
       it 'returns the total number of votes for a publication' do
+        sailor_moon.find_total_votes.must_equal 6
       end
       it 'returns 0 if there are no votes' do
+        publications(:sailorcrystal).find_total_votes.must_equal 0
       end
     end
   end
