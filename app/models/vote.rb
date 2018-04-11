6c050adcb6bb077efd3ea520527a9bac9081893e
Winifred Irarrazaval <winirarrazaval@gmail.com>
2018-04-11 15:42:58 -0700

    fixed a bug about an each loop that had an equal sign

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,6 @@ class Vote < ApplicationRecord
   belongs_to :work
   belongs_to :user
 
-  validates :user_id, :uniqueness => {:scope => :work_id}, message: "user: has already voted for this work" 
+  validates :user_id, :uniqueness => {:scope => :work_id}
 
 end
