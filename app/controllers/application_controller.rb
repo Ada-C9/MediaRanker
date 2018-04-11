d033177bf2db9c75dd512d206e8a534a709f83c7
Phoebe <phoebe.manning8@gmail.com>
2018-04-11 15:48:20 -0700

    fixed application controller method and user form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def find_user
-    @user = User.find_by(id: params[:user_id])
+    @user = User.find_by(id: session[:user_id])
   end
 end
