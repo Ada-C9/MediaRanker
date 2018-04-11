d3ba7abc4b6a59ab99f2ee0aafd79df13a80b7ae
Zheng Cao <chori84@gmail.com>
2018-04-11 15:49:16 -0700

    Fix login/logout bug on user show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:index, :show]
 
   def index
     @users = User.all
+
   end
 
   def show
-    @user = User.find_by(id: params[:id])
+    @this_user = User.find_by(id: params[:id])
   end
 
 end
