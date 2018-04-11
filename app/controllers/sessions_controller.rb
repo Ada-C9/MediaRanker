88394513c3345a2de2a5546829b801aab39a2053
LetiTran <leticiastran@gmail.com>
2018-04-11 14:27:14 -0700

    Added logout feature based on session status

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,14 @@
 class SessionsController < ApplicationController
   def create
     @user = User.find_by name: params[:user][:name]
+
     if @user
       session[:user_id] = @user.id
-      flash[:sucess] = "Welcome back #{@user.name}"
+      flash[:sucess] = "Successfully logged in as existing user #{@user.name}"
     else
       @user = User.create name: params[:user][:name]
       session[:user_id] = @user.id
-      flash[:sucess] = "Welcome #{@user.name}"
+      flash[:sucess] = "Successfully logged in as existing user #{@user.name}"
     end
     redirect_to root_path
   end
