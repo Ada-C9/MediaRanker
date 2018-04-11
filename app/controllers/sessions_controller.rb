e0ef71ebd84dba0a72393f51fc95d28773142e41
Alexandria Brown <axbn2190@gmail.com>
2018-04-11 11:43:18 -0700

    login button created

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_by name: params[:user][:name]
+    @user = User.find_by(name: params[:user][:name])
 
     if @user
       session[:user_id] = @user.id
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -58,6 +58,6 @@ class WorksController < ApplicationController
   end
 
   def find_work
-    @work = Work.find_by(:id params[:id])
+    @work = Work.find_by(id: params[:id])
   end
 end
