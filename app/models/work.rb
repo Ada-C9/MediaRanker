809518666d5893f000d29c8f3585a29ecd361fb7
Nora Peters <norapeters5@gmail.com>
2018-04-11 15:47:08 -0700

    completes new work form with work class method for #valid_categories

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,8 +2,14 @@ class Work < ApplicationRecord
   has_many :votes
   validates_presence_of :title, :category
 
+  # add requirement for uniqueness of title by category?
+
   def vote_count
-    self.votes.where(work_id: self.id).count
+    self.votes.count
+  end
+
+  def self.valid_categories
+    %w[album movie book]
   end
 
 end
