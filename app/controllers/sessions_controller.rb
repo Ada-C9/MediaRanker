d19bd6b3338082497a0da5a3704f98f2e8e18eea
Wenjie <traxiaojie@gmail.com>
2018-04-10 21:23:31 -0700

    Added view template for user show

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def login
-    # user = User.find_by(name: params[:user][:name])
+    user = User.create(name: params[:user][:name])
 
-    # if work
-    #   session[:user_id] = user.id
-    #   flash[:success] = "#{ user.name } is successfully logged in"
-    #   redirect_to root_path
-    # else
+    if user
+      session[:user_id] = user.id
+      flash[:success] = "#{ user.name } is successfully logged in"
+      redirect_to root_path
+    else
     redirect_to user_path(user)
 
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,14 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-
-  def new
-    @user = User.new
-  end
+  #
+  # def new
+  #   @user = User.new
+  # end
 
   def create
     user = User.new(user_params)
-
-    if user.save
-      redirect_to user_path(user)
-    end
+    user.save 
   end
 
   def show
