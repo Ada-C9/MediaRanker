a95d2813635b3ebbe5f9c33f8bf632cdde08d884
Zheng Cao <chori84@gmail.com>
2018-04-10 21:30:50 -0700

    Update Vote model, and implement Vote validations and relations tests

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,9 +1,53 @@
 require "test_helper"
 
 describe Vote do
-  let(:vote) { Vote.new }
 
-  it "must be valid" do
-    value(vote).must_be :valid?
+  describe 'Validations' do
+    # nominal
+    it "is valid when all fields are present" do
+      votes(:one).valid?.must_equal true
+    end
+
+    # presence
+    it "is invalid without a work_id" do
+      votes(:one).work_id = nil
+
+      votes(:one).valid?.must_equal false
+      votes(:one).errors.messages.must_include :work
+    end
+
+    it "is invalid without a user_id" do
+      votes(:one).user_id = nil
+
+      votes(:one).valid?.must_equal false
+      votes(:one).errors.messages.must_include :user
+    end
+
+    # uniqueness
+    it "is invalid if a vote with same work_id and user_id already exists" do
+      vote = Vote.new(work_id: votes(:one).work_id, user_id: votes(:one).user_id)
+
+      vote.valid?.must_equal false
+      vote.errors.messages.must_include :work_id
+    end
   end
+
+  describe 'Relations' do
+    it "has a work" do
+      vote = votes(:one)
+
+      vote.work.must_equal works(:aja)
+    end
+
+    it "has a user" do
+      vote = votes(:one)
+
+      vote.user.must_equal users(:user_1)
+    end
+  end
+
+  describe 'Methods' do
+
+  end
+
 end
