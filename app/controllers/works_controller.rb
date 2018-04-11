a4497bc202f255a55b73741e3efaa758345ecde8
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-04-10 17:12:56 -0700

    Created navigation bar

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,8 +1,15 @@
 class WorksController < ApplicationController
   def index
+    def index
+      @albums = Work.where(category: "album")
+      @books  = Work.where(category: "book")
+      @movies = Work.where(category: "movie")
+    end
+
   end
 
   def show
+    @work = Work.find_by(id: params[:id].to_i )
   end
 
   def new
@@ -18,5 +25,11 @@ class WorksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
   end
 end
