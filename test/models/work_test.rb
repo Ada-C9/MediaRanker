7ff3a8c914482886c2026698bc97917142f79c31
Tor Shimizu <torshimizu@gmail.com>
2018-04-11 12:10:36 -0700

    added tests for ordering and top_work

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -21,7 +21,7 @@ describe Work do
 
     it 'connects vote and vote_id' do
       vote = Vote.create!(user_id: User.first.id, work_id: work.id)
-      
+
       work.vote_ids.must_include vote.id
     end
 
@@ -45,6 +45,38 @@ describe Work do
 
       result.must_equal false
       @work.errors.messages.must_include :title
+    end
+  end
+
+  describe 'model methods' do
+    before do
+      @work1 = works(:backtoblack)
+      @work2 = works(:hill)
+      @work3 = works(:noscrubs)
+
+      @user1 = users(:taco)
+      @user2 = users(:roo)
+
+      @category = Category.find_by(name: 'album')
+
+      Vote.create!(user_id: @user1.id, work_id: @work1.id)
+      Vote.create!(user_id: @user1.id, work_id: @work3.id)
+      Vote.create!(user_id: @user2.id, work_id: @work3.id)
+    end
+
+    it 'can order a category of works by vote' do
+
+      vote_ordered_works = [@work3, @work1, @work2]
+
+      result = Work.order_by_vote(@category.id)
+      result.must_equal vote_ordered_works
+    end
+
+    it 'can get the top work of all media by votes' do
+      top_work = @work3
+      result = Work.top_work
+
+      result.must_equal top_work      
     end
   end
 end
