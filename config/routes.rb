c41c18cd02d3906ff2e22043c3276a73737e81ce
Samantha Berk <sberk10@gmail.com>
2018-04-11 14:59:05 -0700

    Add login form to views and new/login/logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,8 @@ Rails.application.routes.draw do
     resources :votes
   end
 
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#login'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
+
 end
