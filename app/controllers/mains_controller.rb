bfd80eb1f2fc62a87d1fccd6abff350f7dec249c
Zheng Cao <chori84@gmail.com>
2018-04-11 15:30:56 -0700

    Implement Media Spotlight on homepage

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -2,6 +2,9 @@ class MainsController < ApplicationController
   before_action :find_user, only: [:index]
 
   def index
+    @works = Work.all.to_a
+    @top_work = @works.max_by { |work| work.total_votes }
+
     @albums = Work.all.where(category: "album").limit(10).to_a
     @albums.sort_by! { |album| -album.total_votes }
 
