10f33e74485cd6924f3baf49a573d143a9313c90
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-04-11 15:21:08 -0700

    Generate upvotes controller and config route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'upvotes/create'
+
   get '/login', to: 'sessions#new', as:'login_form'
   post '/login', to: 'sessions#create', as:'login'
   delete '/login', to: 'sessions#destroy', as:'logout'
@@ -8,6 +10,11 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
   resources :works
   resources :users, only: [:index, :show]
+
+  resources :works do
+  resources :upvotes, only: [:create]
+end
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
