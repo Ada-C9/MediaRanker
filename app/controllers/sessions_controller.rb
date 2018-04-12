40105896af83b423101c33ab3be9979bb8e43e00
knockknockhusthere <angela.huxia@gmail.com>
2018-04-12 14:08:20 -0700

    fixed create user and update session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,18 +10,20 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       flash[:success] = "#{ @user.name } is successfully logged in."
       redirect_to votes_path
+
     else
       @user = User.new(name: params[:user][:name])
-      session[:user_id] = @user.id
 
       if @user.save
+        session[:user_id] = @user.id
         flash[:success] = "An account has been successfully created. You are now logged on as #{ @user.name }."
+
         redirect_to votes_path
       else
         flash.now[:failure] = "Could not find or create an account, please try again."
         render :new
       end
-      # other logic that does more stuff
+
     end
   end
 
