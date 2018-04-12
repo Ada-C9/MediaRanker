0bffccacbbf7509291e6eafcfecb81cb8b7fc5e8
Alexandria Brown <axbn2190@gmail.com>
2018-04-12 13:21:58 -0700

    fixed links and styling for header

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,20 +3,30 @@ class VotesController < ApplicationController
   end
 
   def index
+    @votes = Vote.all
   end
 
   def new
+    @vote = Vote.new
   end
 
   def show
   end
 
   def create
+    @vote = Vote.find_by(id: params[:id])
+    @vote.update(vote_params)
   end
 
   def destroy
   end
 
   def update
+  end
+
+  private
+
+  def vote_params
+    return params.require(:vote).permit(:user_id, :work_id)
   end
 end
