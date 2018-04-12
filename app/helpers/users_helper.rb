4292478cc279ef7c6a3f08054733ff6afb68cee3
AngelaPoland <angelalpoland@gmail.com>
2018-04-11 18:26:28 -0700

    added one small test for work title validation

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,4 @@
 module UsersHelper
 
-  def created_date(user)
-    raw_date = self.created_at
-    stripped_date = raw_date.strftime("%B %d, %Y")
-    return stripped_date
-  end
 
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,24 @@
 require "test_helper"
 
 describe Work do
-  let(:work) { Work.new }
+  let(:work) { Work.new(title: "Title", category: "book") }
 
   it "must be valid" do
     value(work).must_be :valid?
   end
+
+  it "must not be valid when work has no title" do
+    work.title = nil
+    value(work).wont_be :valid?
+  end
+
+  it "must not be valid when work has empty title" do
+    work.title = " "
+    value(work).wont_be :valid?
+  end
+
+
+
+
+
 end
