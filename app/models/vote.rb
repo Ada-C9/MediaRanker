92fc88cc7d3aae55d5458e3bf3575fdc18f1e773
Steffany Brown <steffannbrown@gmail.com>
2018-04-12 16:02:56 -0700

    added validations to vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,6 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :publication
 
-
+  validates :user_id, numericality: { only_integer: true}
+  validates :publication_id, numericality: { only_integer: true}
 end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -3,13 +3,13 @@ require "test_helper"
 describe Vote do
 
   it "must have a user" do
-    vote = Vote.new
+    vote = Vote.new(publication_id: 1)
     vote.valid?.must_equal false
     vote.errors.must_include :user_id
   end
 
   it "has a publication" do
-    vote = Vote.new
+    vote = Vote.new(user_id: 1)
     vote.valid?.must_equal false
     vote.errors.must_include :publication_id
   end
