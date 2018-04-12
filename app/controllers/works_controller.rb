a7f1938fd37f49753c1ca6b1805ce5cac3dcd0b7
Jamila <J.cornick21@gmail.com>
2018-04-11 22:09:16 -0700

    all media views not working

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -4,10 +4,20 @@ class WorksController < ApplicationController
   end
 
   def new
-
+    @work = Work.new
   end
 
   def create
+    @work = Work.new(work_params)
+
+    if work.save
+      redirect_to work_path(work.id)
+    end
+
+  end
+
+  def edit
+    @work = Work.find(params[:id])
 
   end
 
@@ -15,9 +25,19 @@ class WorksController < ApplicationController
 
   end
 
+  def show
+
+  end
+
   def destroy
 
   end
+
+  private
+  def work_params
+      return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+    end
+
 
 
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,18 @@
 class Work < ApplicationRecord
   has_many :votes
+
+  def self.albums
+    Work.where(id: "album")
+
+  end
+
+  def self.books
+    Work.where(id: "book")
+
+  end
+
+  def self.movies
+    Work.where(id: "movie")
+
+  end
 end
