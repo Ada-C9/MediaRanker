6e8dd8f29e686feaaf3ee5b27f82fdd6530c9378
Phoebe <phoebe.manning8@gmail.com>
2018-04-11 19:36:04 -0700

    creates user links and user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,21 +3,6 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  # def new
-  #   @user = User.new
-  # end
-  #
-  # def create
-  #   @user = User.new(user_params)
-  #   if @user.save
-  #     flash[:success] = "#{@user.name} created"
-  #     redirect_to root_path
-  #   else
-  #     flash[:alert] = {"#{@user.name}" => "creation failed"}
-  #     render :new
-  #   end
-  # end
-
   def show
     @user = User.find_by(id: params[:id])
   end
