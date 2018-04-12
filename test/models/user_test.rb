24e14561e640cb5bd1e1520b846458ec4141b330
Phoebe <phoebe.manning8@gmail.com>
2018-04-12 13:31:53 -0700

    adds validation tests to user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,15 @@
 require "test_helper"
 
 describe User do
+  let(:user1) { users(:user1) }
   describe "validations" do
-    it "is valid when all field are present" do
+    it "is valid when required fields are present" do
+      user1.valid?.must_equal true
+    end
 
+    it "is not valid when required fields are not present" do
+      user1.name = nil
+      user1.valid?.must_equal false
     end
   end
 
