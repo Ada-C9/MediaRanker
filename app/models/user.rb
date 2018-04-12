604f9950690f3e36cd2542329510f36840379809
Samantha Berk <sberk10@gmail.com>
2018-04-11 21:46:36 -0700

    Update User model with upvoted_works relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_many :votes, through: :works
+  has_many :votes
+  has_many :upvoted_works, through: :votes, soure: :work
 
   validates :name, presence: true, uniqueness: true
 
