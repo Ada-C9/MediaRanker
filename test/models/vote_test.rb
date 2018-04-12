58fdcd3e64f2890c97d914ed8c76dcf18519ca1d
Kaitlin Forsman <kcforsman@outlook.com>
2018-04-11 18:02:02 -0700

    added tests for the votes model

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,9 +1,30 @@
 require "test_helper"
 
 describe Vote do
-  let(:vote) { Vote.new }
+  let(:some_vote) { votes(:vote1) }
 
-  it "must be valid" do
-    
+  describe 'Vote validity' do
+    it 'must be valid' do
+      some_vote.valid?.must_equal true
+    end
+    it 'must be valid if user or publication already have a vote' do
+      same_user_vote = Vote.new(user: users(:usako), publication: publications(:spirited))
+      same_publication_vote = Vote.new(user: users(:sailors), publication: publications(:cardcaptor))
+
+      same_user_vote.valid?.must_equal true
+      same_publication_vote.valid?.must_equal true
+    end
+    it 'must not be valid if a vote already exists with both the same user and same publication' do
+      duplicate_vote = Vote.new(user: users(:usako), publication: publications(:cardcaptor))
+
+      duplicate_vote.valid?.must_equal false
+    end
+  end
+
+  describe 'vote relationships' do
+    it 'it has a user and a publication' do
+       some_vote.user.must_equal user: users(:usako)
+       some_vote.publication.must_equal publication: publications(:cardcaptor)
+    end
   end
 end
