b334b2ffa5e83deb1c75e2fe6134f3a2d162adda
Brenda Rios <riosbrenda@gmail.com>
2018-04-12 14:33:28 -0700

    Showing votes.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,37 +5,20 @@ class VotesController < ApplicationController
   end
 
   def create
-    #   @vote = Vote.new(user_id: session[:user_id])
-    #
-    #   if @vote.save
-    #     flash[:status] = :success
-    #     flash[:message] = "Successfully created work #{@vote.id}"
-    #     redirect_to works_path
-    #   else
-    #     # Tell user what went wrong
-    #     flash[:status] = :failure
-    #     flash[:message] = "Could not upvote. User: has already voted for this work"
-    #     flash[:details] = @vote.errors.messages
-    #     redirect_to works_path
-    #   end
-    # end
     user_id = session[:logged_in_user]
-    work_id = params[:id]
+    work_id = params[:work_id]
     @vote = Vote.new(user_id: user_id, work_id: work_id)
 
     if @vote.save
       flash[:status] = :success
       flash[:message] = "Successfully upvoted!"
-      redirect_back(fallback_location: root_path)
+      redirect_to works_path
     else
-      if @vote.user_id ==  nil
-        flash[:message] = "You must loggin to do that"
-      else
-        flash[:status] = :failure
-        flash[:message] = "Could not upvote.You have already voted for this work"
-      end
+
+      flash[:status] = :failure
+      flash[:message] = "Could not upvote.You have already voted for this work"
+      redirect_to works_path
     end
-    redirect_back(fallback_location: root_path)
   end
 
   private
