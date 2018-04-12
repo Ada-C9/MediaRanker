b464c07895d2540f081136f3ab70f0128d78396d
Kiera <kiera.thomasson@gmail.com>
2018-04-11 19:06:51 -0700

    Updated user controller with CRUD. Added the log in button to the nav bar.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,45 +1,55 @@
 class UsersController < ApplicationController
 
   def index
-    @users = user.all
+    @users = User.all
   end
 
   def new
-    @user = user.new
+    @user = User.new
   end
 
   def create
-    user = user.new(user_params)
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'User dded successfully'
+      redirect_to root_path
+    else
+      flash.now[:failure] = 'Validations Failed'
+      render :new
+    end
   end
 
   def show
     user_id = params[:id]
 
-    @user = user.find(user_id)
+    @user = User.find(user_id)
   end
 
   def edit
-    @user = user.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def update
-    user = user.find(params[:id])
+    @user = User.find(params[:id])
 
-    user.assign_attributes(user_params)
+    @user.assign_attributes(user_params)
 
-    if user.save
+    if @user.save
       redirect_to user_path(user)
+    else
+      render :edit
     end
   end
 
   def destroy
-    @user = user.find(params[:id])
+   User.destroy(params[:id])
 
     redirect_to users_path
   end
 
   private
+
   def user_params
-    return params.require(:user).permit(:name)
+    return params.require(:user).permit(:name, :created_at)
   end
 end
