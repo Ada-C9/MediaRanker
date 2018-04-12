c5095898cf7eed6fb1faad56282740b0ea222152
Steffany Brown <steffannbrown@gmail.com>
2018-04-12 13:14:46 -0700

    updated sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,6 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(name: params[:user][:name])
-
     if @user
       session[:user_id] = @user.id
       flash[:success] = "Successfully logged in as existing user #{@user.name}"
