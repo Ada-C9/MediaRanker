436eab026abe773f3f5fffff39fcb94d14cb539b
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-04-11 23:38:41 -0700

    mechanics for reporting top and spotlighted media works in place and functional.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,5 +4,6 @@ class ApplicationRecord < ActiveRecord::Base
   def report_vote_tally(id_for_work)
     @media_vote_num = Vote.where(work_id: id_for_work).count
   end
-  
+
+
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -4,4 +4,27 @@ class Work < ApplicationRecord
   validates :title, presence: true
   validates :title, length: { in: 1..75 }
 
+
+  def self.works_with_vote_tallies
+    works = self.all
+    all_tallies = {}
+    works.each do |work|
+      work_tally_hash = {work => work.report_vote_tally(work.id)}
+      all_tallies.merge!(work_tally_hash)
+    end
+    sorted_tallies = all_tallies.sort_by {|k, v| v }.reverse.to_a
+    return sorted_tallies
+  end
+
+  def self.get_top_ten_works_with_tallies
+    top_ten = works_with_vote_tallies.first(10)
+    return top_ten
+  end
+
+  def self.get_top_work_with_tally
+    top = works_with_vote_tallies.first(1)
+    raise
+    return top
+  end
+
 end
