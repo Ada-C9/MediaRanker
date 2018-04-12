a211f139f8d71833071eb0fb7f753e3440c7b5e5
Phoebe <phoebe.manning8@gmail.com>
2018-04-12 13:16:41 -0700

    adds logged in as link if a user is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :find_user
-  
+
   def find_user
     @user = User.find_by(id: session[:user_id])
-    # @user = User.find_by name: params[:user][:name]
   end
 
-  # helper_method: current_user
-  # def current_user
-  #   session[:user_id]
-  # end
+  helper_method :current_user
+  def current_user
+    session[:user_id]
+  end
 end
