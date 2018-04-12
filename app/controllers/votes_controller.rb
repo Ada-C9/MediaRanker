aec1c19b99eb3093e374dd008bd6b57db52affee
Karinna Iniguez <karinnainiguez@gmail.com>
2018-04-12 14:21:40 -0700

    Add partial and render for new and edit works views

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,10 +1,12 @@
 class VotesController < ApplicationController
 
   def upvote
+
     if session[:user_id]
       user = User.find(session[:user_id])
       work = Work.find(params[:id])
       new_vote = Vote.new(user: user, work: work)
+
       if new_vote.save
         flash[:success] = "Successfully upvoted!"
         redirect_back(fallback_location: root_path)
@@ -12,9 +14,11 @@ class VotesController < ApplicationController
         flash[:failure] = "Oops! Something went wrong.  Try again"
         redirect_back(fallback_location: root_path)
       end
+
     else
       flash[:failure] = "You must log in to do that"
       redirect_back(fallback_location: root_path)
     end
+
   end
 end
