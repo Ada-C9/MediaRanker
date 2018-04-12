5f4574c334930bb9b3acdbfbcbf2a97a2a345209
Samantha Berk <sberk10@gmail.com>
2018-04-11 17:40:27 -0700

    Move user created date to a helper method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,4 @@ class User < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
 
-  def created_date
-    raw_date = self.created_at
-    stripped_date = raw_date.strftime("%B %d, %Y")
-    return stripped_date
-  end
-
 end
