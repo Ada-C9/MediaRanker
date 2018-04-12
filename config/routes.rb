40cbd07c09fd2cafd0e5098391f9ad091f12071c
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-04-11 21:50:39 -0700

    Added sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  resources :users, :only => [:index, :show, :new, :create]
+  resources :users, except: [:edit]
+
   resources :works
+
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
+
 end
