1e6d67c258e53efb987641176a85724d26296497
Steffany Brown <steffannbrown@gmail.com>
2018-04-12 13:57:39 -0700

    added get_votes method to publication model, updated publications index view to display votes for each publication

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -6,4 +6,11 @@ class Publication < ApplicationRecord
 
   validates :publication_year, presence: true, length: { is: 4 }
   validates :publication_year, numericality: { only_integer: true, less_than_or_equal_to: 2018 }
+
+  def get_votes(publication_id)
+    votes = Vote.where(id: publication_id)
+    number_votes = votes.count
+    return number_votes
+  end
+
 end
