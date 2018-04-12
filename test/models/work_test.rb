8538f115d06fd371613295efc10bc6a1172219b7
Samantha Berk <sberk10@gmail.com>
2018-04-11 18:22:52 -0700

    Small adjustment to syntax of tests for Work model

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -11,7 +11,7 @@ describe Work do
     it "can be created with all required fields" do
       result = @work.valid?
 
-      result.must_equal true
+      value(result).must_equal true
     end
 
     it "is invalid without a category" do
@@ -19,7 +19,7 @@ describe Work do
 
       result = @work.valid?
 
-      result.must_equal false
+      value(result).must_equal false
     end
 
     it "is invalid without a title" do
@@ -27,14 +27,14 @@ describe Work do
 
       result = @work.valid?
 
-      result.must_equal false
+      value(result).must_equal false
     end
 
     it "is invalid with a duplicate title and category" do
       duplicate_work = Work.new(category: 'album', title: 'test title', creator: 'another creator', publication_year: 2000)
 
       result = duplicate_work.valid?
-      result.must_equal false
+      value(result).must_equal false
       duplicate_work.errors.messages.must_include :title, :creator
     end
 
@@ -42,7 +42,7 @@ describe Work do
       duplicate_work = Work.new(category: 'book', title: 'test title', creator: 'another creator', publication_year: 2000)
 
       result = duplicate_work.valid?
-      result.must_equal true
+      value(result).must_equal true
     end
 
     it "is invalid without a creator" do
