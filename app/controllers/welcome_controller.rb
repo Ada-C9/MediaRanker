efa213134f126b5f8d201a3bcd70561c9cea8c8b
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-04-11 23:29:59 -0700

    Add top 10

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     @user = User.find_by(id: session[:user_id])
-    @movies = Work.all.where(category: "movie").limit(10)
-    @books = Work.all.where(category: "book").limit(10)
-    @albums = Work.all.where(category: "album").limit(10)
+    @albums = Work.top(10, "album")
+    @books = Work.top(10, "book")
+    @movies = Work.top(10, "movie")
   end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -10,10 +10,16 @@ class Work < ApplicationRecord
 
   private
   def publication_year_must_be_in_the_past
-    if !publication_year.nil?
-      if publication_year > Date.today.year
-        errors.add(:publication_year, "cannot be in the future")
-      end
+    if !publication_year.nil? && publication_year > Date.today.year
+      errors.add(:publication_year, "cannot be in the future")
     end
+  end
+
+  def self.top(count, category)
+    works = self.where(category: category)
+    sorted_works = works.sort_by { |work| work.upvotes.count }.reverse!
+    actual_count = [count, sorted_works.count].min
+    top_works = sorted_works[0...actual_count]
+    return top_works
   end
 end
