0f97abc4ea9432c8a46f676ec384c2d31dd2cb73
0velez0 <mariaivelez@gmail.com>
2018-04-12 16:46:05 -0700

    create partial form, add create logic, index and main views

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,19 +1,38 @@
 class WorksController < ApplicationController
 
+
+  def main
+    @works = Work.all
+    @albums = Work.albums
+    @movies = Work.movies
+    @books = Work.books
+  end
+
   def index
     @works = Work.all
-
+    @albums = Work.albums
+    @movies = Work.movies
+    @books = Work.books
   end
 
   def show
-  @work = Work.find_by(id: params[:id].to_i)
+    @work = Work.find_by(id: params[:id].to_i)
   end
 
   def new
+    @work = Work.new
   end
 
   def create
-
+    @work = Work.new(work_params)
+    if @work.save
+      flash[:success] = "Work added successfully"
+      redirect_to works_path
+    else
+    
+      flash.now[:failure] = "Validations Failed"
+      render :new
+    end
   end
 
   def edit
@@ -29,7 +48,6 @@ class WorksController < ApplicationController
 
   # TODO undestand this
   def work_params
-    return
     params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'works/index'
+  # root 'works#index'
+  root 'works#main'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :works  
+  resources :works
+
+  resources :users
+
 end
