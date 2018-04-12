d21b703a82c34a3a6531df8cfee3c698ff3555e0
SelamawitA <Selamawit.ainalem@gmail.com>
2018-04-11 18:41:58 -0700

    added button to user show page that allows a re-direct to all users. Added another button that will re-direct back to list of all media

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
 
-
   root 'homepage#index'
-
   get '/works/new', to: 'works#new', as:'new_work'
   post '/works', to:'works#create', as: 'works'
   get '/works', to: 'works#index'
@@ -10,6 +8,10 @@ Rails.application.routes.draw do
   get '/works/:id/edit', to:'works#edit', as:'edit_work'
   delete '/works/:id', to: 'works#destroy', as:'delete_work'
   patch '/works/:id', to:'works#update'
+
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show', as: 'user'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
