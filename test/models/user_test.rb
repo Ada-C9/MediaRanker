d4f2a4bde413271cbfc8d857eb7fa81db5317a25
Kaitlin Forsman <kcforsman@outlook.com>
2018-04-11 17:09:04 -0700

    created validation tests for User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,26 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
+  let(:usachan) { users[:usako] }
 
-  it "must be valid" do
-    value(user).must_be :valid?
+  describe 'user validity' do
+    it 'must be valid' do
+      usachan.valid?.must_equal true
+    end
+    it 'must have a name' do
+      usachan.name = nil
+
+      usachan.valid?.must_equal false
+    end
+    it 'cant have an empty string as a name' do
+      usachan.name = ""
+
+      usachan.valid?.must_equal false
+    end
+    it 'has to have a unique name' do
+      duplicate_user = User.new(name: "Usagi")
+
+      usachan.valid?.must_equal false
+    end
   end
 end
