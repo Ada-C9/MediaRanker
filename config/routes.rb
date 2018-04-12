0b361fba1168a23bff7058b8002fa12fc84b5f81
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-04-12 14:18:50 -0700

    Set up sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'sessions/create'
+
+  get 'sessions/new'
+
+  get 'sessions/destroy'
+
   root 'welcomes#index'
   
   resources :users, only: [:index, :show]
