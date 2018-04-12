fc3466fb833e236dd9ee1d5d609270063f24d37c
LetiTran <leticiastran@gmail.com>
2018-04-12 10:09:33 -0700

    Working on making a new vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -6,11 +6,21 @@ class VotesController < ApplicationController
   end
 
   def new
-    
+    @vote = Vote.new
+      @vote.creator = Work.find(params[:creator_id])
   end
 
   def create
+    @vote = Vote.new(work_id: @work.id)
+    @vote.creator = Work.find(params[:creator_id])
 
+    if @work.save
+      flash[:sucess] = "Vote saved"
+      redirect_to works_path
+    else
+      flash.now[:alert] = "Media was not save."
+      render :new
+    end
   end
 
   def edit
