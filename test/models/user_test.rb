c46a8e6d733aeb3cba6852a213c080dc5a07d19c
Kiera <kiera.thomasson@gmail.com>
2018-04-11 19:25:04 -0700

    Added validations to the user model.Added a link for users to login. Added render partial for error messages within user log in form. Added fixtures and validation tests that are passing.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,34 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
+  describe 'validations' do
+    before do
+      @user = User.new(name:User.first)
+    end
 
-  it "must be valid" do
-    value(user).must_be :valid?
+    it "can be created with required fields" do
+      result = @user.valid?
+
+      result.must_equal true
+    end
+
+    it "is invalid without a user name" do
+      @user.name = nil
+
+      result = @user.valid?
+
+      result.must_equal false
+      @user.errors.messages.must_include :name
+    end
+
+    it "is invalid with a duplicate name" do
+      duplicate = User.first
+
+      @user.name = duplicate.name
+      result = @user.valid?
+
+      result.must_equal false
+      @user.errors.messages.must_include :name
+    end
   end
 end
