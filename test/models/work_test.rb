58f78886d1ea54fd6883c14021cb2514f5432245
marikoja <Marikoanderson@gmail.com>
2018-04-12 13:34:00 -0700

    created fixtures

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,13 @@
 require "test_helper"
 
 describe Work do
-  let(:work) { Work.new }
+  let(:work) { Work.new(category: "album", title: "Are You Experienced", creator: "The Jimi Hendrix Experience", publication_year: 1967) }
 
   it "must be valid" do
     value(work).must_be :valid?
+  end
+
+  it "must not be valid when category is not selected" do
+    
   end
 end
