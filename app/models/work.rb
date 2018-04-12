89f36dc37acdd2b25d48d7130ce7710f814c9341
Brenda Rios <riosbrenda@gmail.com>
2018-04-12 14:27:02 -0700

    Fixed bug on top-ten works.

--- app/models/work.rb
+++ app/models/work.rb
@@ -18,21 +18,21 @@ class Work < ApplicationRecord
 
   def self.top_ten_albums
     album = Work.where(category: "album")
-    return album.sort_by {|work| work.votes.count }.take(10)
+    return album.sort_by {|work| work.votes.count }.reverse.take(10)
   end
 
   def self.top_ten_movies
     movie = Work.where(category: "movie")
-    return movie.sort_by {|work| work.votes.count }.take(10)
+    return movie.sort_by {|work| work.votes.count }.reverse.take(10)
   end
 
   def self.top_ten_books
     book = Work.where(category: "book")
-    return book.sort_by {|work| work.votes.count }.take(10)
+    return book.sort_by {|work| work.votes.count }.reverse.take(10)
   end
 
   def self.top
-    sorted = Work.all.sort_by{|work| work.votes.count}
+    sorted = Work.all.sort_by{|work| work.votes.count}.reverse
     top= sorted.first
     return top
   end
