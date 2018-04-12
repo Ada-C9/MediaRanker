62919b9ba7d416ba55daa58b6669d7baf2ed51cf
AngelaPoland <angelalpoland@gmail.com>
2018-04-12 13:49:26 -0700

    created sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'sessions/create'
+
+  get 'sessions/new'
+
+  get 'sessions/destroy'
+
   root 'welcomes#index'
 
   resources :users
@@ -11,31 +17,3 @@ Rails.application.routes.draw do
   # resources :votes
 
 end
-
-
-# Prefix Verb   URI Pattern               Controller#Action
-#      root GET    /                         welcome#index
-#     users GET    /users(.:format)          users#index
-#           POST   /users(.:format)          users#create
-#  new_user GET    /users/new(.:format)      users#new
-# edit_user GET    /users/:id/edit(.:format) users#edit
-#      user GET    /users/:id(.:format)      users#show
-#           PATCH  /users/:id(.:format)      users#update
-#           PUT    /users/:id(.:format)      users#update
-#           DELETE /users/:id(.:format)      users#destroy
-#     works GET    /works(.:format)          works#index
-#           POST   /works(.:format)          works#create
-#  new_work GET    /works/new(.:format)      works#new
-# edit_work GET    /works/:id/edit(.:format) works#edit
-#      work GET    /works/:id(.:format)      works#show
-#           PATCH  /works/:id(.:format)      works#update
-#           PUT    /works/:id(.:format)      works#update
-#           DELETE /works/:id(.:format)      works#destroy
-#     votes GET    /votes(.:format)          votes#index
-#           POST   /votes(.:format)          votes#create
-#  new_vote GET    /votes/new(.:format)      votes#new
-# edit_vote GET    /votes/:id/edit(.:format) votes#edit
-#      vote GET    /votes/:id(.:format)      votes#show
-#           PATCH  /votes/:id(.:format)      votes#update
-#           PUT    /votes/:id(.:format)      votes#update
-#           DELETE /votes/:id(.:format)      votes#destroy
