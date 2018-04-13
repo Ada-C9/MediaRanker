b30cbdc87a2888a59c33ed43455ce195daf07efb
Anne <annehwatson@gmail.com>
2018-04-13 11:36:45 -0700

    some user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,5 +1,31 @@
 require "test_helper"
 
 describe User do
-  
+  describe "validations" do
+    before do
+      @user = User.new(name: "Anne")
+    end
+
+    it "can be created with all required fields" do
+      result = @user.valid?
+      result.must_equal true
+    end
+
+    it "is invalid without a name" do
+      @user.name = nil
+
+      result = @user.valid?
+
+      result.must_equal false
+      @user.errors.messages.must_include :name
+    end
+  end
+
+  describe "relations" do
+    before do
+      @user = User.new(name: "Anne")
+    end
+
+    
+  end
 end
