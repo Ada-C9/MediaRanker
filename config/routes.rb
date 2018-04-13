98c74c0ca3f07fdce589225d687e8a628889b4da
Kate Pond <kate@thepondsedge.com>
2018-04-13 13:29:56 -0700

    adds votes buttons and some controller logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'homepage#index'
 
+  post 'works/:id/upvote', to:"works#upvote", as: 'upvote'
+
   resources :works
   resources :users, only: [:index, :show, :new, :create]
   resources :votes, only: [:create]
