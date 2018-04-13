4c6db22eabd51be32a3112fb405981f476bc521d
0velez0 <mariaivelez@gmail.com>
2018-04-13 14:50:00 -0700

    create user list view index

--- db/migrate/20180413035707_create_users.rb
+++ db/migrate/20180413035707_create_users.rb
@@ -2,8 +2,6 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :name
-      t.date :join_date
-      t.string :summary
 
       t.timestamps
     end
