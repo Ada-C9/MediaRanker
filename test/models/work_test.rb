b2435cc44ad3acd6648d6f516bb817fc8b4b48eb
Samantha Berk <sberk10@gmail.com>
2018-04-12 22:14:24 -0700

    Tests for works relationships and order methods

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -63,4 +63,92 @@ describe Work do
 
   end
 
+  describe  "relationships" do
+
+    before do
+      @work = works(:album)
+    end
+
+    it "has many votes" do
+
+      @work.votes.each do |vote|
+        vote.must_be_kind_of Vote
+      end
+
+    end
+
+    it "has a list of voting users" do
+
+      @work.upvoted_users.each do |user|
+        user.must_be_kind_of User
+      end
+
+    end
+
+  end
+
+  describe "self.order_albums" do
+
+    it "returns a list of works with the category album" do
+      album = works(:album)
+      result = Work.order_albums
+
+      result.must_include album
+      result.length.must_equal 1
+      result.each do |work|
+        work.category.must_equal "album"
+      end
+    end
+  end
+
+  describe "self.order_books" do
+
+    it "returns a list of works with the category book" do
+      book = works(:book)
+      result = Work.order_books
+
+      result.must_include book
+      result.length.must_equal 1
+      result.each do |work|
+        work.category.must_equal "book"
+      end
+
+    end
+
+  end
+
+  describe "self.order_movies" do
+
+    it "returns a list of works with the category movie" do
+      movie = works(:movie)
+      result = Work.order_movies
+
+      result.must_include movie
+      result.length.must_equal 1
+      result.each do |work|
+        work.category.must_equal "movie"
+      end
+
+    end
+
+  end
+
+  # describe "self.find_spotlight" do
+  #
+  #   it "returns the work with the most overall votes" do
+  #     album = works(:album)
+  #     book = works(:book)
+  #     movie = works(:movie)
+  #
+  #     album.votes.length = 5
+  #     book.votes.length = 1
+  #     movie.vote.length = 0
+  #
+  #     result = Work.find_spotlight
+  #
+  #     result.must_equal album
+  #   end
+  #
+  # end
+
 end
