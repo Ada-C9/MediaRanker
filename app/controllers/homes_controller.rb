c0ae31f4f011f588ead849a5f91ddf6b447da46a
Maja Graonic <m.graonic@gmail.com>
2018-04-13 12:18:41 -0700

    guard against nil on homepage top media overall

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,7 @@
 class HomesController < ApplicationController
   def index
     @top_work = Work.top_overall
+    
     @albums = Work.top('album')
     @books = Work.top('book')
     @movies = Work.top('movie')
