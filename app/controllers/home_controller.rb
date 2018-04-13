608473399de2b2fc8516f61566b02dfe3f8e1fbe
LetiTran <leticiastran@gmail.com>
2018-04-13 15:31:19 -0700

    Added css classes acording to the original MediaRanker provided.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,12 @@
 class HomeController < ApplicationController
 
-  before_action :sort_by_votes 
+  before_action :sort_by_votes
 
   def index
+    @spotlight = Work.first
+    # if top != nil
+    #   top = @spotlight
+    # end
     @albums = @media[0]
     @movies = @media[1]
     @books = @media[2]
