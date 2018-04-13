468720e257400e14d05e6315c2442ccdf54bcfd0
Kat <kat.eastman@gmail.com>
2018-04-12 18:39:58 -0700

    added logic for new media by category.

--- app/helpers/works_helper.rb
+++ app/helpers/works_helper.rb
@@ -2,16 +2,20 @@ module WorksHelper
 
 def category_from_url(fullpath)
   case fullpath
-  when "/books"
+  when "/books", "/books/new"
     @content = [@books]
     @content_title = ["Books"]
-  when "/movies"
+  when "/movies", "/movies/new"
     @content = [@movies]
     @content_title = ["Movies"]
-  when "/albums"
+  when "/albums", "/albums/new"
     @content = [@albums]
     @content_title = ["Albums"]
+  when "/works/new"
+    render :new
   else
+    @content = [@movies, @books, @albums]
+    @content_title = ["Movies", "Books", "Albums"]
   end
 end
 
