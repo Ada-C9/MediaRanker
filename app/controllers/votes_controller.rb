ffac2020fdce088a9c5dc978e8bdc546b449101f
Nora Peters <norapeters5@gmail.com>
2018-04-13 11:04:57 -0700

    implements votes#create and adds before_action :find_user"

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,4 +1,7 @@
 class VotesController < ApplicationController
+
+  before_action :find_user
+
   def index
   end
 
@@ -9,10 +12,18 @@ class VotesController < ApplicationController
   end
 
   def create
-    # @vote = Vote.new
-    # @vote.work_id = Work.find_by(id: params[:work_id]).id
-    # @vote.user_id = 
+    @vote = Vote.new
+    @vote.work_id = Work.find_by(id: params[:work_id]).id
+    @vote.user_id = @user.id
+    if @vote.save
+      flash[:success] = "Successfully upvoted!"
+      redirect_back fallback_location: :works_path
+    else
+      flash.now[:alert] = @vote.errors
+      render :new
+    end
   end
+
     # need to add conditional that checks if user is logged in, in order to be able to upvote...and use flash notices to remind user to login first
 
   def edit
@@ -23,5 +34,7 @@ class VotesController < ApplicationController
 
   def destroy
   end
+
+
 
 end
