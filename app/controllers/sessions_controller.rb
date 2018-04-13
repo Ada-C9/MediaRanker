f30a7dfcae7d92bdd0a3bef867f8a27ea7e3f970
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-04-12 20:07:19 -0700

    session controller is looking better but still broken, need help. Moving on to votes for a bit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,38 @@
 class SessionsController < ApplicationController
+  def login_form; end
 
   def new
     @user = User.new
   end
 
-  def create
-    @user = User.find_by(user_name: params[:user_name])
-    # FIXME: NEED THIS BIT TO SUCCESSFULLY DISPLAY USER_NAME and redirect to top media page********
-    if @user
-      session[:user_id] = @user.id
-      flash[:success] = "#{@user.user_name}is successfully logged in"
-      @users << @user
-      redirect_to root_path
+  def login
+    user_name = params[:user_name]
+
+    if user_name and user = User.find_by(user_name: user_name)
+      session[:user_id] = user_id
+      flash[:satus] = :success
+      flash[:result_text] = "Successfully logged in as exsisting user #{user.user_name}"
+    else
+      user = User.new(user_name: user_name)
+      if user.save
+        session[:user_id] = user_id
+        flash[:status] = :success
+        flash[:result_text] = "Successfully created new user #{user.user_name} with ID #{user.id}."
+        # FIXME: WHY DOES IT NOT LIKE CREATE
+      else
+        flash.now[:status] = :failure
+        falsh.now[:message] = user.errors.messages
+        render "login_form", status: :bad_request
+        return
+      end
     end
-    # ********************************
+    redirect_to root_path
   end
 
   def destroy
-    session.delete(:user_id)
-    flash[:success] = "Logged out successfully."
+    session[:user_id] = nil
+    flash[:status] = :success
+    flash[:result_text] = "Logged out successfully."
 
     redirect_to root_path
   end
