a0edbdf3fcd2720467f86cc3d10b7b2ef7b0e776
Anne <annehwatson@gmail.com>
2018-04-12 20:39:35 -0700

    populated yaml files

--- app/models/work.rb
+++ app/models/work.rb
@@ -5,9 +5,17 @@ class Work < ApplicationRecord
   validates :title, presence: true
 
   def self.top_ten_movies
-    @top_ten_movies = []
-    @top_ten_movies = self.order(votes.count).first(10)
+    movies = Work.where(category: "movie")
+
+    @top_ten_movies = movies.first(10)
     return @top_ten_movies
+  end
+
+  def self.top_ten_books
+    books = Work.where(category: "book")
+
+    @top_ten_books = books.first(10)
+    return @top_ten_books
   end
 
   def most_popular
