29458c07098e85293bdc49f447ff914455160136
Steffany Brown <steffannbrown@gmail.com>
2018-04-12 18:22:52 -0700

    styled main#index and updated publications#index to link titles to show pages

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -2,9 +2,12 @@ class MainsController < ApplicationController
   before_action :find_user
 
   def index
-    @albums = Publication.where(category: "album").limit(10)
-    @books = Publication.where(category: "book").limit(10)
-    @movies = Publication.where(category: "movie").limit(10)
+    albums = Publication.where(category: "album")
+    @albums = albums.left_joins(:votes).group(:id).order('COUNT(votes.id) DESC').limit(10)
+    books = Publication.where(category: "book")
+    @books = books.left_joins(:votes).group(:id).order('COUNT(votes.id) DESC').limit(10)
+    movies = Publication.where(category: "movie")
+    @movies = movies.left_joins(:votes).group(:id).order('COUNT(votes.id) DESC').limit(10)
     @spotlight = Publication.first
   end
 end
