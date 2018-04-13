80ddd56ce7fb7aafa6b369a6db74f20ac45c3d0c
Abiaina <abinnet.a@gmail.com>
2018-04-13 15:47:19 -0700

    added votes column to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180413224140) do
+ActiveRecord::Schema.define(version: 20180413224529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180413224140) do
     t.date "joindate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "votes"
   end
 
   create_table "votes", force: :cascade do |t|
