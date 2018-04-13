9c097dc8c9207e0ca8214474cab635b4cee7b5f8
Phoebe <phoebe.manning8@gmail.com>
2018-04-13 15:04:10 -0700

    created migrations for removing columns from user and vote models

---
+++ db/migrate/20180413220318_remove_voted_date_from_votes.rb
@@ -0,0 +1,5 @@
+class RemoveVotedDateFromVotes < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :votes, :voted_date, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180410045228) do
+ActiveRecord::Schema.define(version: 20180413220318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.date "joined"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -25,7 +24,6 @@ ActiveRecord::Schema.define(version: 20180410045228) do
   create_table "votes", force: :cascade do |t|
     t.bigint "work_id"
     t.bigint "user_id"
-    t.datetime "voted_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_votes_on_user_id"
