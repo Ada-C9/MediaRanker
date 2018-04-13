b1cb03ada885708be6be020cc6ff29a7fa5eb545
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-04-12 22:38:52 -0700

    Added foreign keys and attempt to fix login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,17 @@
 class SessionsController < ApplicationController
 
   def create
-
     @user = User.find_by user_name: params[:user][:name]
+
+
     if @user
       session[:user_id] = @user.id
-      flash[:success] = "Welcome back #{@user.user_name}"
     else
-      @user = User.create user_name: params[:user][:user_name]
+      @user = User.create user_name: params[:user][:name]
       session[:user_id] = @user.id
-      flash[:success] = "Welcome #{@user.user_name}"
     end
     redirect_to root_path
   end
-
 
   def new
     @user = User.new
@@ -21,8 +19,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    flash[:success] = "You logged out!"
     redirect_to root_path
   end
-
 end
