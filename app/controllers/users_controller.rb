b6767edc563c0d9840bafa2cadbb1dc422f8ae55
Kate Pond <kate@thepondsedge.com>
2018-04-13 14:44:11 -0700

    removes unneeded files, and creates views for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all.order(:created_at)
   end
 
-  def new
-  end
-
-  def create
+  def show
+    @user = User.find_by(id: params[:id])
+    if @user == nil
+      redirect_to "/404.html"
+    end
   end
 end
