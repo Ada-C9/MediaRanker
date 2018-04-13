f864aaa6d24fb139027c3a4f6d38b7f7a71aff05
Aruna <aruna.thungathurthi@gmail.com>
2018-04-12 20:19:20 -0700

    added flash to upvote method

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,5 +1,5 @@
 class WorksController < ApplicationController
-  before_action :find_work, only: [:show, :edit, :update,:destroy]
+  before_action :find_work, only: [:show, :edit, :update,:destroy,:upvote]
 
   def index
     @works = Work.all
@@ -31,41 +31,56 @@ class WorksController < ApplicationController
 
   def update
     if @work.update(work_params)
-        redirect_to work_path(@work.id)
-      else
-        render :edit
-      end
+      redirect_to work_path(@work.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
-    if @work
-      if @work
-        @work.destroy
-      else
-      flash[:success] = "#{@work.title} deleted"
+    user = nil
+    if session[:user_id]
+      user =  User.find_by(id: session[:user_id])
+      @work = Work.find_by(id:params[:id])
+      @work.destroy
+    else
+      flash[:status] = "Deleted "
+      redirect_to works_path
     end
-      flash[:alert] = {work: "does not exist"}
-    end
-    redirect_to root_path
+    flash[:alert] = {work: "does not exist"}
+
+    redirect_to works_path
 
   end
   def upvote
-    @vote = Vote.new
-    @vote.user = User.find(session[:user_id])
-    @vote.work = Work.find_by(id: params[:id])
-    @vote.date =  Date.today
-    if @vote.save
-      redirect_to root_path
-    else
-
+    if !(session[:user_id])
+      flash[:status] = :failure
+      flash[:message] = "you must be logged in to do that"
+      redirect_back fallback_location: {action: "index"}
+    elsif  session[:user_id]
+      @vote = Vote.new
+      @vote.user = User.find(session[:user_id])
+      @vote.work = Work.find_by(id: params[:id])
+      @vote.date =  Date.today
+      if @vote.save
+        flash[:status] = :success
+        flash[:message] = "Successfully upvoted "
+        redirect_back fallback_location: {action: "index"}
+      else
+        flash[:status] =:failure
+        flash[:message]= "Could not upvote.#{@user.username} has already upvoted for "
+        redirect_back fallback_location: {action: "index"}
+      end
     end
   end
+
   private
   def work_params
     return params.require(:work).permit(:category, :title, :created, :publication_year, :description)
   end
   def find_work
     @work = Work.find_by(id: params[:id])
+
   end
 
 end
