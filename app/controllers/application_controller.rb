6e0fb613744eb8d84559033b8bd1ad6a9a8cc30a
Nora Peters <norapeters5@gmail.com>
2018-04-13 11:00:58 -0700

    modified app#find_user method to set user to current session user if active

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def find_user
-    @user = User.find_by(id: session[:user_id])
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    end
   end
 
 end
