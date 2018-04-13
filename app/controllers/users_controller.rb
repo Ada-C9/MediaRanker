4413fd2df090b54e7cbfe3fb4d60b172816ec124
LetiTran <leticiastran@gmail.com>
2018-04-13 11:40:15 -0700

    Fixed #show to show the selected user instead of the logged in user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
 
-  before_action :find_user
+  before_action :find_active_user
 
   def index
     @users = User.all
   end
 
   def show
-
+    @user = User.find(params[:id])
   end
 
   def new
