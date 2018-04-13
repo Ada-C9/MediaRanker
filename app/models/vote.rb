a7e83cb86ca7b36ef8dc4eb8f2c31bb773117487
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-04-12 17:31:27 -0700

    Added and passing validation tests for votes model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,6 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :work
 
-  validates :user_id, uniqueness: { scope: :work_id }
+  validates :work_id, presence: true
+  validates :user_id, presence: true, uniqueness: { scope: :work_id }
 end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,9 +1,59 @@
 require "test_helper"
 
 describe Vote do
-  let(:vote) { Vote.new }
+  describe "validations" do
+    before do
+      @existing_vote = Vote.create!(user_id: User.last.id, work_id: Work.last.id)
+      @vote = Vote.new(user_id: User.first.id, work_id: Work.first.id)
+    end
 
-  it "must be valid" do
-    value(vote).must_be :valid?
+    it "can be created with all required fields" do
+      result = @vote.valid?
+
+      result.must_equal true
+    end
+
+    it "is invalid without a user_id" do
+      @vote.user_id = nil
+
+      result = @vote.valid?
+
+      result.must_equal false
+      @vote.errors.messages.must_include :user_id
+    end
+
+    it "is invalid without a work_id" do
+      @vote.work_id = nil
+
+      result = @vote.valid?
+
+      result.must_equal false
+      @vote.errors.messages.must_include :work_id
+    end
+
+    it "can be created with a duplicate work_id and different user_id" do
+      @vote.work_id = @existing_vote.work_id
+
+      result = @vote.valid?
+
+      result.must_equal true
+    end
+
+    it "can be created with a duplicate user_id and different work_id" do
+      @vote.user_id = @existing_vote.user_id
+
+      result = @vote.valid?
+
+      result.must_equal true
+    end
+
+    it "is invalid with a duplicate user_id and work_id" do
+      @vote.work_id = @existing_vote.work_id
+      @vote.user_id = @existing_vote.user_id
+
+      result = @vote.valid?
+
+      result.must_equal false
+    end
   end
 end
