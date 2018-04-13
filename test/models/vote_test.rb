fe136b7e4670ef4b11505a731afac8a4ca39fae4
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-04-12 18:05:49 -0700

    Added and passing tests for votes relations

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -3,8 +3,8 @@ require "test_helper"
 describe Vote do
   describe "validations" do
     before do
-      @existing_vote = Vote.create!(user_id: User.last.id, work_id: Work.last.id)
-      @vote = Vote.new(user_id: User.first.id, work_id: Work.first.id)
+      @existing_vote = Vote.create!(user: User.last, work: Work.last)
+      @vote = Vote.new(user: User.first, work: Work.first)
     end
 
     it "can be created with all required fields" do
@@ -13,47 +13,65 @@ describe Vote do
       result.must_equal true
     end
 
-    it "is invalid without a user_id" do
-      @vote.user_id = nil
+    it "is invalid without a user" do
+      @vote.user = nil
 
       result = @vote.valid?
 
       result.must_equal false
-      @vote.errors.messages.must_include :user_id
+      @vote.errors.messages.must_include :user
     end
 
-    it "is invalid without a work_id" do
-      @vote.work_id = nil
+    it "is invalid without a work" do
+      @vote.work = nil
 
       result = @vote.valid?
 
       result.must_equal false
-      @vote.errors.messages.must_include :work_id
+      @vote.errors.messages.must_include :work
     end
 
-    it "can be created with a duplicate work_id and different user_id" do
-      @vote.work_id = @existing_vote.work_id
+    it "can be created with a duplicate work and different user" do
+      @vote.work = @existing_vote.work
 
       result = @vote.valid?
 
       result.must_equal true
     end
 
-    it "can be created with a duplicate user_id and different work_id" do
-      @vote.user_id = @existing_vote.user_id
+    it "can be created with a duplicate user and different work" do
+      @vote.user = @existing_vote.user
 
       result = @vote.valid?
 
       result.must_equal true
     end
 
-    it "is invalid with a duplicate user_id and work_id" do
-      @vote.work_id = @existing_vote.work_id
-      @vote.user_id = @existing_vote.user_id
+    it "is invalid with a duplicate user and work" do
+      @vote.work = @existing_vote.work
+      @vote.user = @existing_vote.user
 
       result = @vote.valid?
 
       result.must_equal false
+    end
+  end
+
+  describe "relations" do
+    it "connects user and user_id" do
+      user = User.first
+
+      vote = Vote.create!(user: user, work: Work.first)
+
+      vote.user_id.must_equal user.id
+    end
+
+    it "connects work and work_id" do
+      work = Work.first
+
+      vote = Vote.create!(user: User.first, work: work)
+
+      vote.work_id.must_equal work.id
     end
   end
 end
