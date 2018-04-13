92971759dff7db02276e47a80fda47b99c1c1424
Alexandria Brown <axbn2190@gmail.com>
2018-04-12 22:27:00 -0700

    work#show buttons, upvote link, # of votes

--- app/models/work.rb
+++ app/models/work.rb
@@ -3,4 +3,15 @@ class Work < ApplicationRecord
 
   validates :category, presence: true
   validates :title, length: { minimum: 1, maximum: 25 }, allow_blank: false
+
+  def self.top_work(works)
+    highest = works.first
+    works.each do |work|
+      if work.votes.count > highest.votes.count
+        highest = work
+      end
+    end
+    return highest
+  end
+
 end
