5fb26262eaf612605026c3252416f1e78143b329
Samantha Berk <sberk10@gmail.com>
2018-04-13 11:21:31 -0700

    Remove unnecessary vote_count column from works table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180412204815) do
+ActiveRecord::Schema.define(version: 20180413181959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 20180412204815) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "vote_count"
     t.index ["title", "category"], name: "index_works_on_title_and_category", unique: true
   end
 
