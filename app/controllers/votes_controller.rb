3b4c70d84d21da4ed25511d433ca547b2a73eeb9
Dikla <dikla.aharoni@gmail.com>
2018-04-12 18:46:42 -0700

    Updated main page

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -10,18 +10,18 @@ class VotesController < ApplicationController
   def create
     if session[:user_id]
 
-    new_vote = {
-      user_id: session[:user_id],
-      work_id: params[:work_id]
-    }
-    @vote = Vote.new(new_vote)
+      new_vote = {
+        user_id: session[:user_id],
+        work_id: params[:work_id]
+      }
+      @vote = Vote.new(new_vote)
 
-    if @vote.save
-      flash[:notice] = "Successfully upvoted!"
-    else
-      flash[:alert] = "Could not upvote"
-      flash[:notice] = @vote.errors.full_messages
-    end
+      if @vote.save
+        flash[:notice] = "Successfully upvoted!"
+      else
+        flash[:alert] = "Could not upvote"
+        flash[:notice] = @vote.errors.full_messages
+      end
     end
     redirect_to request.referer
   end
