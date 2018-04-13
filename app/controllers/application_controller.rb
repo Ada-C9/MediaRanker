4390c29917637bcdd3abd0ff86cb184c7432aec1
LetiTran <leticiastran@gmail.com>
2018-04-13 11:41:20 -0700

    Deleted the flash feature since it wasnt working properly - will add it again later

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :find_user
-  def find_user
+
+  before_action :find_active_user
+
+  def find_active_user
     @user = User.find_by(id: session[:user_id])
   end
 end
