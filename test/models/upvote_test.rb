a5afb97694ddacb57f10e6e850cec2fae242d550
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-04-12 23:03:54 -0700

    add upvote tests

--- test/models/upvote_test.rb
+++ test/models/upvote_test.rb
@@ -14,11 +14,24 @@ describe Upvote do
   end
 
   it "checks for validation with missing id" do
-  vote.work_id = nil
-  vote.valid?.must_equal false
-  vote.user_id = nil
-  vote.valid?.must_equal false
-end
+    vote.work_id = nil
+    vote.valid?.must_equal false
+    vote.errors.messages.must_include :work
 
+    vote.user_id = nil
+    vote.valid?.must_equal false
+    vote.errors.messages.must_include :user
+  end
 
+  it "has validation to ensure a user can upvote only once on a work " do
+    vote1 = Upvote.new{upvotes(:one_up)}
+    vote1.valid?.must_equal false
+    vote1.errors.messages.must_include :work
+    vote1.errors.messages.must_include :user
+  end
+
+  it "belongs to a work and user" do
+    vote.user.must_equal users(:one)
+    vote.work.must_equal works(:hp)
+  end
 end
