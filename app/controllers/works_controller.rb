38c415a9d6e87d6a3fa3efa777d93696ecc09a20
Nora Peters <norapeters5@gmail.com>
2018-04-13 20:43:09 -0700

    implements works#destroy and before_action find_user for edit and update

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,5 +1,7 @@
 class WorksController < ApplicationController
 
+  before_action :find_user, only: [:edit, :update]
+
   def index
     @movies = Work.where(category: 'movie')
     @books = Work.where(category: 'book')
@@ -27,11 +29,9 @@ class WorksController < ApplicationController
   end
 
   def edit
-    @work = Work.find_by(id: params[:id])
   end
 
   def update
-    @work = Work.find_by(id: params[:id])
     if @work
       if @work.update(work_params)
         flash[:success] = "#{@work.title} updated"
@@ -43,7 +43,27 @@ class WorksController < ApplicationController
   end
 
   def destroy
+    @work = Work.find_by(id: params[:id])
+
+    if @work
+      if !@work.votes.empty?
+        @work.votes.each do | vote |
+          vote.destroy
+        end
+      end
+      if @work.destroy
+        flash[:success] = "Successfully destroyed #{@work.category} #{@work.title}"
+        redirect_to root_path
+      else
+        flash.now[:alert] = { "delete #{@work.category}" => @work.errors }
+        render :show
+      end
+    else
+      flash[:alert] = "Work does not exist"
+      redirect_to root_path
+    end
   end
+
 
   def work_params
     return params.require(:work).permit(:category, :title, :creator, :publication_year)
