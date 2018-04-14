e7935f982cfaa8ce13e411ff36bbb3a6cf39ba24
Aruna <aruna.thungathurthi@gmail.com>
2018-04-13 21:55:23 -0700

    finished work model testing

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,40 +1,114 @@
 require "test_helper"
 
+
 describe Work do
 
+  describe "validations" do
+    # success validation case for the working model
+    it "can be created with all required fields" do
+      work = Work.create!(category: "album", title: "book11", creator: "author" ,publication_year: 1960,description:"good book")
+      work.must_be :valid?
+    end
 
-  it "must be valid" do
-    work = Work.new(:category=>"album",:title=> "liquidspirit")
-    work.must_be :valid?
+    # failure validation cases
+    it "requires a title" do
+      w = Work.new
+      w.wont_be :valid?
+      w.errors.messages.must_include :title
+    end
+
+    it "requires a category" do
+      w = Work.new
+      w.wont_be :valid?
+      w.errors.messages.must_include :category
+    end
   end
 
-  it "reqiures a title" do
-    work = Work.new
-    work.wont_be :valid?
-    work.errors.messages.must_include :title
+  describe "relations" do
+    it "responds to votes and can be update the vote " do
+      # arrange
+      w = works(:book9)
+      w.must_respond_to :votes
+      w.votes.must_be :empty?
+      w.votes.count.must_equal 0
+      vote = Vote.create!(date: Date.today, work_id: w.id, user_id: users(:user1).id)
+
+      # act
+      w.votes << vote
+
+      #assert
+      w.votes.count.must_equal 1
+      w.votes.must_include vote
+    end
+
+    it "can have multiple votes" do
+      # arrange
+      w = works(:book9)
+      w.votes.must_be :empty?
+      w.votes.count.must_equal 0
+
+      vote1 = Vote.create!(date: Date.today, work_id: w.id, user_id: users(:user1).id)
+      w.votes << vote1
+
+      vote2 = Vote.create!(date: Date.today, work_id: w.id, user_id: users(:user2).id)
+
+      # act
+      w.votes << vote2
+
+      #assert
+      w.votes.count.must_equal 2
+      w.votes.must_include vote1
+      w.votes.must_include vote2
+    end
+
+    # Test dependent destroy relationship
+    it "can destroy dependent relationship to vote" do
+      # arrange
+      w = works(:book3)
+
+      #
+      # act
+      w.destroy!
+
+      #assert
+      Vote.all.wont_include works(:book3)
+
+    end
   end
 
-  it "requires a category" do
-    work = Work.new
-    work.wont_be :valid?
-
-    work.errors.messages.must_include :category
+  describe "custom public methods" do
+    describe "top_work" do
+    it "returns top voted work" do
+      # top voted work in fixtures is Hunger
+      w = works(:hunger)
+      Work.top_work.must_equal w
+    end
   end
 
+  describe "top_ten" do
+    it "returns the top ten media in a category that has more than 10 items" do
+      # top voted books in fixtures are Hunger, book3, Kindred, and book4 in that order the rest of the books have zero votes
+      top_books = Work.top_ten("book")
+
+      #top_books.count.must_equal 10
+
+      top_books.first.must_equal works(:hunger)
+    end
+
+    it "returns all media in a category that has fewer than 10 items" do
+      # there are only two albums and they are Lemonade and Liquid Spirit in that order by votes
+      top_albums = Work.top_ten("album")
+
+      top_albums.count.must_equal 2
+
+      top_albums.must_equal [works(:book3), works(:book4)]
+    end
+
+    it "returns an empty array if category has no works" do
+      top_albums = Work.top_ten("movie")
+      top_albums.must_be :empty?
+    end
+  end
 end
 
-describe "relations" do
-  it "responds to votes and can be voted on once " do
-    work =works(:book1)
-    binding.pry
-    work.must_respond_to :votes
-    work.votes.must_be :empty?
-    work.votes.must_equal 0
-    vote = Vote.create(work_id: work.id,user_id: users(:user1).id,date:2018-04-13)
-
-    work.votes << vote
-
-    work.votes.count.must_equal 1
-    work.votes.must_include vote
-  end
-  end
+end
