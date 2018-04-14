c1abbc8da3e2341d68f0cfccb5a669f222aa8833
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-04-14 00:04:44 -0700

    Created methods for spotlight in work controller and created html for welcome page

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,7 +1,11 @@
 class WelcomesController < ApplicationController
+  before_action :find_user, only: [:index]
+
   def index
-    @albums = Work.where(category: "album")
-    @books  = Work.where(category: "book")
-    @movies = Work.where(category: "movie")
+    @top_work = Work.top_1
+
+    @albums = Work.top_10("album")
+    @books = Work.top_10("book")
+    @movies = Work.top_10("movie")
   end
 end
