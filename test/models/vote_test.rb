af59b7c8204799dad76f5a59b90889dddce91ffc
AngelaPoland <angelalpoland@gmail.com>
2018-04-13 18:26:18 -0700

    votes tests

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,22 +1,37 @@
 require "test_helper"
 
 describe Vote do
-  let(:vote) { Vote.new }
+  let(:vote) { votes(:one) }
 
   it "must be valid" do
-    skip
-    value(vote).must_be :valid?
+    vote.must_be :valid?
   end
+
+  it "must have a unique user/work per category" do
+    vote_one = votes(:one)
+    vote_two = votes(:seven)
+    vote_two.work = works(:book_one)
+    vote_two.valid?.must_equal false
+  end
+
+  it "is invalid without both work and user ids" do
+    votes(:four).work_id = nil
+    votes(:four).user_id = nil
+
+    votes(:four).valid?.must_equal false
+    votes(:four).errors.messages.must_include :work
+    votes(:four).errors.messages.must_include :user
+  end
+
 
   describe 'relations' do
 
     it 'must belong to a work' do
-      skip
+
       votes(:one).work.must_equal works(:book_one)
     end
 
     it 'must belong to a user' do
-      skip
       votes(:two).user.must_equal users(:angela)
     end
 
