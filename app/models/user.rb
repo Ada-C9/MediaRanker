4d2b1c6858215c401e4c49c6f0107fcf7410929c
Nora Peters <norapeters5@gmail.com>
2018-04-13 23:46:30 -0700

    adds additional validation to user model. user model tests written and passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :votes
   validates_presence_of :username
+
+  validates :username, uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,6 +4,25 @@ describe User do
   let(:user) { User.new }
 
   it "must be valid" do
-    value(user).must_be :valid?
+    nora = users(:nora)
+    value(nora).must_be :valid?
   end
+
+  it "must have a collection of votes" do
+    user.votes.must_equal []
+    vote = Vote.new
+    user.votes << vote
+    user.votes.must_equal [vote]
+
+    users(:nora).votes.length.must_equal 2
+  end
+
+  it "must have a username" do
+    user.must_respond_to :username
+    users(:chad).username.must_equal "Chad"
+
+    user.valid?.must_equal false
+    user.errors.must_include :username
+  end
+
 end
