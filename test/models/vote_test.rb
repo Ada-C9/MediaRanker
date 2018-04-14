da3776345947b3c45cd0106e606a7cd9d2ca700a
Nora Peters <norapeters5@gmail.com>
2018-04-13 21:13:37 -0700

    adds 2 tests to votes, updates test_helper with gems etc

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -3,7 +3,28 @@ require "test_helper"
 describe Vote do
   let(:vote) { Vote.new }
 
-  it "must be valid" do
+  it "must be valid" do\
     value(vote).must_be :valid?
+  end
+
+  it "does not allow voting twice" do
+    user = nil
+    work = nil
+
+    vote = Vote.create!(user: user, work: work)
+    vote2 = Vote.new(user: user, work: work)
+
+    expect(vote2.valid?).to eq(false)
+
+    expect(vote2.errors[:user])
+      .to include("You cannot vote for same work twice.")
+  end
+
+  it 'can assign a user to a vote' do
+    user = User.create!(username: "Nora")
+    vote = Vote.new(user: user)
+
+    vote.user_id.must_equal user.id
+
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,10 +14,23 @@ Minitest::Reporters.use!(
 
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
-# require "minitest/rails/capybara"
+require "minitest/rails/capybara"
+require 'capybara/minitest/spec'
+
+require 'capybara-screenshot/minitest'
+
+Capybara::Screenshot.autosave_on_failure = true
+
+class ActionDispatch::IntegrationTest
+  include Capybara::Screenshot::MiniTestPlugin
+end
+
+require 'capybara/poltergeist'
+Capybara.default_driver = :poltergeist
+Capybara.javascript_driver = :poltergeist
 
 # Uncomment for awesome colorful output
-# require "minitest/pride"
+require "minitest/pride"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
