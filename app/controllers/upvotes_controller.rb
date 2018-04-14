865a73ab11edb5486b0478359c2f03250be621f3
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-04-14 12:09:12 -0700

    reinstate upvotes functionality

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -1,36 +1,59 @@
 class UpvotesController < ApplicationController
   before_action :find_user
-  def new
-    @upvote = Upvote.new
-    @upvote.user = User.find(session[:user_id])
-    @upvote.work = Work.find(params[:work_id])
-  end
+  before_action :find_work, only: [:show, :edit, :update, :destroy]
 
-  def index
-    @user = User.find_by(id: session[:user_id] )
-    if params[:work_id]
-      work = Work.find_by(id: params[:work_id])
-      @upvotes = user.books
-    else
-      @upvote = Upvote.all.order(params[:id])
-    end
-  end
-
-  def show
-    id = params[:id]
-    @upvote = Upvote.all.find(params[:id])
-  end
+  # def new(current_work.id)
+  #   # user_id = session[:user_id]
+  #   @upvote = Upvote.new
+  #   # @upvote.current_user = User.find(session[:user_id])
+  #   @upvote.work_id = current_work.id
+  # end
+  #
+  # def index
+  #   @user = User.find_by(id: session[:user_id] )
+  #   if params[:work_id]
+  #     work = Work.find_by(id: params[:work_id])
+  #     @upvotes = user.books
+  #   else
+  #     @upvote = Upvote.all.order(params[:id])
+  #   end
+  # end
+  #
+  # def show
+  #   id = params[:id]
+  #   @upvote = Upvote.all.find(params[:id])
+  # end
 
   def create
-    @upvote = Upvote.new
-    if @upvote.save
-      @upvote.user_id = session[:user_id]
-      @upvote.work_id = params[:work_id]
-      redirect_to upvote_path(@upvote.id)
+    if !@user
+      flash[:alert] = {user: "You must be logged in to vote"}
+      redirect_back fallback_location: :works_path
     else
-      render :new
+      @vote = Upvote.create
+      @vote.work_id = Work.find_by(id: params[:work_id]).id
+      @vote.user_id = @user.id
+      if @vote.save
+        flash[:success] = "Successfully upvoted!"
+        redirect_back fallback_location: :works_path
+
+      else
+        flash[:alert] = @vote.errors
+        redirect_to root_path
+      end
+
     end
   end
+
+  # def create
+  #   @upvote = Upvote.new
+  #   if @upvote.save
+  #     @upvote.user_id = session[:user_id]
+  #     @upvote.work_id = params[:work_id]
+  #     redirect_to upvote_path(@upvote.id)
+  #   else
+  #     render :new
+  #   end
+  # end
 
   def upvote_params
     params.require(:upvote).permit(:work_id, :user_id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,28 @@
 Rails.application.routes.draw do
 
-  root to: 'works#welcome'
-  resources :works do
-      resources :upvotes, :except => [:destroy, :edit, :update]
-  end
-
-  resources :users, :except => [:destroy, :edit] do
-    resources :upvotes, :only => [:show, :index]
-  end
-  
-  resources :upvotes, :except => [:destroy, :edit, :update]
 
   get '/login', to: 'sessions#new', as: 'login_form'
   post '/login', to: 'sessions#create', as: 'login'
   delete '/login', to: 'sessions#destroy', as: 'logout'
+
+  root to: 'works#welcome'
+
+  resources :works do
+    resources :upvotes, only: [:create]
+  end
+
+  resources :users, only: [:index, :show]
+  #
+  # resources :works do
+  #   resources :upvotes, :except => [:destroy, :edit, :update]
+  # end
+  #
+  # resources :users, :except => [:destroy, :edit] do
+  #   resources :upvotes, :only => [:show, :index]
+  # end
+  #
+  # resources :upvotes, :except => [:destroy, :edit, :update]
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
