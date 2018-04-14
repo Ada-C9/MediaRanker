0f1fb06f6cb3afa9293520bdd5cb4e039be2b321
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-04-14 00:15:22 -0700

    Added validation for user uniqueness

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,5 @@
 class Work < ApplicationRecord
-  has_many :votes
+  has_many :votes, dependent: :destroy
   validates :title, presence: true
   validates :category, presence: true
   validates :category, inclusion: { in: %w(movie book album)}
