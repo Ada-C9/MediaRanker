70b2b5a5d6b5ccae51b822645e3e82c4d57b0788
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-04-14 23:37:00 -0700

    cleaned up test files

--- test/models/upvote_test.rb
+++ test/models/upvote_test.rb
@@ -15,7 +15,6 @@ describe Upvote do
       result = upvote.valid?
       result.must_equal true
     end
-
   end
 
   it 'must have a work' do
@@ -28,6 +27,31 @@ describe Upvote do
     upvote = upvotes(:up_eighteen)
     upvote.valid?.must_equal false
     upvote.errors.must_include :user
+  end
+
+  it "must not let a user vote for the same work twice in a day" do
+    puts "_________start duplication test____________"
+    upvote_a = upvotes(:up_nineteen)
+    upvote_b = upvotes(:up_twenty)
+    upvote_b.valid?.must_equal false
+    puts upvote_a.valid?
+    puts upvote_a.user_id
+    puts upvote_a.created_at.strftime("%B %d, %Y")
+    puts upvote_a.errors
+
+    puts
+    puts upvote_b.valid?
+    puts upvote_b.user_id
+    puts upvote_b.created_at
+
+    puts
+    puts upvote_a.created_at - upvote_b.created_at
+    puts
+    puts today_date = Time.now
+    puts  upvote_b.created_at
+    puts today_date - upvote_b.created_at
+
+    puts "_________end duplication test____________"
   end
 
 end
