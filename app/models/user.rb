ab5807cdad34c4871d1bc20872bedc0fb827b10f
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-04-14 18:13:28 -0700

    Added test for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :votes
-  validates :user_name, presence: true, uniqueness:true
+  validates :user_name, presence: true, uniqueness: true
 
   def total_votes
     self.votes.count
