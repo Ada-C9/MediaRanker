5e4091d10950a54b7ad508f8d65d6579b41b0b17
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-04-14 19:24:58 -0700

    small update to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+csv_text = Rails.root.join('db', 'media_seeds.csv')
+
+
+import_failures = []
+i = 1
+CSV.foreach(csv_text, :headers => true) do |row|
+  work = Work.new
+  work.id = i
+  work.work_category = row['work_category']
+  work.work_title = row['work_title']
+  work.work_creator = row['work_creator']
+  work.work_publication_year = row['work_publication_year']
+  work.work_description = row['work_description']
+  successful = work.save
+  puts "Created work: #{work.inspect}"
+  if !successful
+    import_failures << work
+  end
+  i += 1
+end
+
+puts "There are now #{Work.count} rows in the transactions table"
+puts "Unfortunately, #{import_failures.length} works failed to save"
