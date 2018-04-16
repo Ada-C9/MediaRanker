86d069337891d1e05ac15e0da461ba6b3bdc0163
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-04-16 16:48:54 -0700

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def find_user
-    @user = User.find_by(id: session[:user_id])
+    @login_user = User.find_by(id: session[:user_id])
   end
 
   def find_work
