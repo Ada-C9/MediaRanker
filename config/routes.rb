a8aa0809a8c779470c95a34a3bfc1aba2f6e0e90
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-04-15 23:01:21 -0700

    DRY up routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root 'top_tens#index'
 
   resources :works
-  resources :users
+  resources :users, only: [:index, :show]
 
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
