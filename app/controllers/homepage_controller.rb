3dc6a77033822461506258f087deccad8b1f0244
Kate Pond <kate@thepondsedge.com>
2018-04-15 17:56:53 -0700

    methods for work on the home page working

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,8 @@
 class HomepageController < ApplicationController
   def index
+    @books = Work.order_works("book")[0 .. 10]
+    @albums = Work.order_works("album")[0 .. 10]
+    @movies = Work.order_works("movie")[0 .. 10]
+    # raise
   end
 end
