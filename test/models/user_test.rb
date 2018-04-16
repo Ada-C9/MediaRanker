6c541de20c023a6528217991a8e143f7b42a158b
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-04-15 18:59:20 -0700

    Added more data to fixtures and included relations test for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 describe User do
   describe "validations" do
     before do
-      @user= User.new(username: "me")
+      @user = User.new(username: "me")
     end
 
     it "can be created with all required fields" do
@@ -29,6 +29,27 @@ describe User do
 
       result.must_equal false
       @user.errors.messages.must_include :username
+    end
+  end
+
+  describe "relations" do
+    before do
+      @user = User.create!(username: "me")
+    end
+
+    it "connects votes and vote_ids" do
+      vote = Vote.create!(user: @user, work: Work.first)
+
+      @user.votes.must_include vote
+      @user.vote_ids.must_include vote.id
+    end
+
+    it "connects works and work_ids" do
+      fav_work = works(:graceland)
+      vote = Vote.create!(user: @user, work: fav_work)
+
+      @user.works.must_include fav_work
+      @user.work_ids.must_include fav_work.id
     end
   end
 end
