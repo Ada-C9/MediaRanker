4d8b16639fda9006d629aea7f96a725dc05231f9
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-04-16 20:15:08 -0700

    test login method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -30,7 +30,6 @@ class SessionsController < ApplicationController
 
   def logout
     session[:user_id] = nil
-    flash[:status] = :success
     flash[:result_text] = "Successfully logged out"
     redirect_to root_path
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
 
+  root to: 'works#welcome'
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#logout', as: 'logout'
-
-  root to: 'works#welcome'
 
   resources :works do
     resources :upvotes, only: [:create, :show, :new, :index]
