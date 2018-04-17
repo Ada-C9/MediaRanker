3f220f5b419c54699f8ccb70ed0971f8101b2def
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-04-16 21:07:23 -0700

    try to fix logout path

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
   root to: 'works#welcome'
+
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
-  patch '/logout', to: 'sessions#update', as: 'logout'
+  post '/logout', to: 'sessions#logout', as: 'logout'
+
 
   resources :works do
     resources :upvotes, only: [:create, :show, :new, :index]
