619db8337e89b5607d5392aac765472627d94b3c
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-04-16 19:35:08 -0700

    removed extraneous code in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,14 @@ class SessionsController < ApplicationController
     @current_user = User.new
   end
 
-  def login
-    user = User.find_by(username: params[:username])
-    if user
-      session[:user_id] = user.id
-      flash[:success] = "#{ user.username } is successfully logged in"
-      redirect_to root_path
-    end
-  end
+  # def login
+  #   user = User.find_by(username: params[:username])
+  #   if user
+  #     session[:user_id] = user.id
+  #     flash[:success] = "#{ user.username } is successfully logged in"
+  #     redirect_to root_path
+  #   end
+  # end
 
   def create
     # username = params[:username]
