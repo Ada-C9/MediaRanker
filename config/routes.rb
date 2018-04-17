2dd6cdee6d24a4184b8aac69b8b8b5759c3c05c3
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-04-16 19:27:12 -0700

    fix routes to match methods nominclature

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new', as: 'login_form'
   post '/login', to: 'sessions#create', as: 'login'
-  delete '/login', to: 'sessions#destroy', as: 'logout'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
 
   root to: 'works#welcome'
 
