39e64b7d09ba8f197d4d0a19ddc942c8cec1b5aa
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-04-18 21:39:31 -0700

    removed duplication in routes and add erd

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,17 @@
 Rails.application.routes.draw do
-  get 'upvotes/create'
+  # get 'upvotes/create'
 
   get '/login', to: 'sessions#new', as:'login_form'
   post '/login', to: 'sessions#create', as:'login'
   delete '/login', to: 'sessions#destroy', as:'logout'
-
-  get 'welcome/index'
 
   root to: 'welcome#index'
   resources :works
   resources :users, only: [:index, :show]
 
   resources :works do
-  resources :upvotes, only: [:create]
-end
-
+    resources :upvotes, only: [:create]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
